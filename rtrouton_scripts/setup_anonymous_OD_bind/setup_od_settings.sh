fa4c46f863a08b5c3c6cb23ef0d412b935ddb4c3
Rich Trouton <rtrouton@yahoo.com>
2011-05-15 21:42:51 -0400

    Posting script to clear old OD settings and setup new anoymous bind OD settings.

---
+++ rtrouton_scripts/setup_anonymous_OD_bind/setup_od_settings.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+####################################################################
+# Delay the login window to give the new DS settings time to apply #
+####################################################################
+
+defaults write /Library/Preferences/com.apple.loginwindow StartupDelay -int 120
+
+#######################################
+## Wait for network services to be up #
+#######################################
+
+sleep 60
+
+#################################################
+##  	Remove old OD settings   	       ##
+#################################################
+
+rm -rf /Library/Preferences/DirectoryService
+killall DirectoryService
+
+#################################################
+##  Add the OD settings for OD Server Name     ##
+#################################################
+
+dsconfigldap -v -s -a od.server.name 
+dscl -q localhost -create /Search SearchPolicy dsAttrTypeStandard:CSPSearchPath
+dscl -q localhost -merge /Search CSPSearchPath /LDAPv3/nhgri-mcx.nih.gov
+killall DirectoryService
+
+################################ 
+# Remove the loginwindow delay #
+################################
+
+defaults delete /Library/Preferences/com.apple.loginwindow StartupDelay
+
+
+# Remove setup LaunchDaemon item
+
+srm /Library/LaunchDaemons/com.nhgri.setup_od_settings.plist 
+
+# Make script self-destruct
+
+srm $0
