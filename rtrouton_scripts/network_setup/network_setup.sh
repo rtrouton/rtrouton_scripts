7134a63effe84225b16a8bce99a00537c1acba66
Rich Trouton <rtrouton@yahoo.com>
2011-05-16 21:00:06 -0400

    DNS server IP and DNS search domain setup script

---
+++ rtrouton_scripts/network_setup/network_setup.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+SearchDomains="searchdomain1.com searchdomain2.com searchdomain3.com"
+
+# Install DNS servers and DNS search domains
+
+/usr/sbin/networksetup -setdnsservers "Built-in Ethernet" dns.ip1.here dns.ip2.here
+/usr/sbin/networksetup -setsearchdomains "Built-in Ethernet" $SearchDomains
+/usr/sbin/networksetup -setdnsservers "Ethernet" dns.ip1.here dns.ip2.here
+/usr/sbin/networksetup -setsearchdomains "Ethernet" $SearchDomains
+/usr/sbin/networksetup -setdnsservers "Ethernet 1" dns.ip1.here dns.ip2.here
+/usr/sbin/networksetup -setsearchdomains "Ethernet 1" $SearchDomains
+/usr/sbin/networksetup -setdnsservers "Ethernet 2" dns.ip1.here dns.ip2.here
+/usr/sbin/networksetup -setsearchdomains "Ethernet 2" $SearchDomains
+/usr/sbin/networksetup -setdnsservers "Airport" dns.ip1.here dns.ip2.here
+/usr/sbin/networksetup -setsearchdomains "Airport" $SearchDomains
+
+
+# Remove setup LaunchDaemon item
+
+srm /Library/LaunchDaemons/com.company.networksetup.plist
+
+# Make script self-destruct
+
+srm $0
