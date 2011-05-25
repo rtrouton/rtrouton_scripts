25a9cb35a020afc6b8f32f618a41dc7e1ed6d4cb
Rich Trouton <rtrouton@yahoo.com>
2011-05-24 20:59:01 -0400

    Adding first boot script to set Energy Saver settings.

---
+++ rtrouton_scripts/setting_energy_saver_settings/README.txt
@@ -0,0 +1,9 @@
+This script is a first boot script that sets separate power management settings for desktops and laptops.
+
+If it's a laptop, the power management settings for "Battery" are set to have the computer sleep in 15 minutes, disk will spin down 
+in 10 minutes, the display will sleep in 5 minutes and the display itslef will dim to half-brightness before sleeping. While plugged 
+into the AC adapter, the power management settings for "Charger" are set to have the computer never sleep, the disk doesn't spin down, 
+the display sleeps after 30 minutes and the display dims before sleeping.
+
+If it's not a laptop (i.e. a desktop), the power management settings are set to have the computer never sleep, the disk doesn't spin down, 
+the display sleeps after 30 minutes and the display dims before sleeping.
---
+++ rtrouton_scripts/setting_energy_saver_settings/powersettings.sh
@@ -0,0 +1,36 @@
+
+#!/bin/sh
+
+#
+# Energy Saver Settings Example for Mac OS X 10.5.x - 10.6.x
+# Rich Trouton, created September 18 2009
+# Last modified March 5, 2011
+#
+
+# Set separate power management settings for desktops and laptops
+# If it's a laptop, the power management settings for "Battery" are set to have the computer sleep in 15 minutes, disk will spin down 
+# in 10 minutes, the display will sleep in 5 minutes and the display itself will dim to half-brightness before sleeping. While plugged 
+# into the AC adapter, the power management settings for "Charger" are set to have the computer never sleep, the disk doesn't spin down, 
+# the display sleeps after 30 minutes and the display dims before sleeping.
+# 
+# If it's not a laptop (i.e. a desktop), the power management settings are set to have the computer never sleep, the disk doesn't spin down, 
+# the display sleeps after 30 minutes and the display dims before sleeping.
+#
+
+# Detects if this Mac is a laptop or not by checking the model ID for the word "Book" in the name.
+IS_LAPTOP=`/usr/sbin/system_profiler SPHardwareDataType | grep "Model Identifier" | grep "Book"`
+
+if [ "$IS_LAPTOP" != "" ]; then
+	pmset -b sleep 15 disksleep 10 displaysleep 5 halfdim 1
+	pmset -c sleep 0 disksleep 0 displaysleep 30 halfdim 1
+else	
+	pmset sleep 0 disksleep 0 displaysleep 30 halfdim 1
+fi
+
+# Remove setup LaunchDaemon item
+
+srm /Library/LaunchDaemons/com.company.powersettings.plist
+
+# Make script self-destruct
+
+srm $0
