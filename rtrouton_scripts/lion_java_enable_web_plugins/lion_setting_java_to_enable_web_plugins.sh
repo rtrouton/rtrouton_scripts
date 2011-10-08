38463bd98d33dac4811edc9f185865d0d5551569
Rich Trouton <rtrouton@yahoo.com>
2011-10-08 10:23:54 -0400

    Updating the lion_setting_java_to_enable_web_plugins script to include creating the ByHost directory.
    Updating the lion_setting_java_to_enable_web_plugins script to have the
    script creates the ByHost directory in the system's user template if it
    doesn't already exist.

--- rtrouton_scripts/lion_java_enable_web_plugins/lion_setting_java_to_enable_web_plugins.sh
+++ rtrouton_scripts/lion_java_enable_web_plugins/lion_setting_java_to_enable_web_plugins.sh
@@ -3,6 +3,11 @@
 # Get the system's UUID to set ByHost prefs
 MAC_UUID=$(system_profiler SPHardwareDataType | awk -F" " '/UUID/{print $3}')
 
+# Creates the ByHost directory in the system's user template
+# if it doesn't already exist.
+
+mkdir /System/Library/User\ Template/English.lproj/Library/Preferences/ByHost/
+
 # Activates the "Enable applet plug-in and Web Start Applications" setting in Java Preferences
 
 /usr/libexec/PlistBuddy -c "Add :GeneralByTask:Any:WebComponentsEnabled bool true" /System/Library/User\ Template/English.lproj/Library/Preferences/ByHost/com.apple.java.JavaPreferences.$MAC_UUID.plist
