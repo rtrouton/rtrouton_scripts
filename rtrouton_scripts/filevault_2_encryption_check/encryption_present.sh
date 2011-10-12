b7791270fb705f02d0495009f9b365eb331d62c5
Rich Trouton <rtrouton@yahoo.com>
2011-10-12 11:10:38 -0400

    Improved the cleanup of temp files in the FileVault 2 encryption check script.

--- rtrouton_scripts/filevault_2_encryption_check/encryption_present.sh
+++ rtrouton_scripts/filevault_2_encryption_check/encryption_present.sh
@@ -91,6 +91,17 @@ if [ "$OS" = "10.7" ]; then
       fi  
 fi
 fi
-rm $ENCRYPTDIRECTION
-rm $ENCRYPTSTATUS
-rm $CORESTORAGESTATUS
+
+# Remove the temp files created during the script
+
+if [ -f /private/tmp/corestorage.txt ]; then
+   srm /private/tmp/corestorage.txt
+fi
+
+if [ -f /private/tmp/encrypt_status.txt ]; then
+   srm /private/tmp/encrypt_status.txt
+fi
+
+if [ -f /private/tmp/encrypt_direction.txt ]; then
+   srm /private/tmp/encrypt_direction.txt
+fi
