a524f6e02ac9c23374fb8373b117ceb1f3029541
Rich Trouton <rtrouton@yahoo.com>
2011-10-12 10:28:03 -0400

    FileVault 2 encryption check script cleaned up and more functional. and better comm
    The FileVault 2 encryption check script has been cleaned up, is more
    functional and is better commented. It now checks to see if a Mac is
    running 10.7, whether or not it is encrypted, and gives the encryption
    or decryption status.

--- rtrouton_scripts/filevault_2_encryption_check/encryption_present.sh
+++ rtrouton_scripts/filevault_2_encryption_check/encryption_present.sh
@@ -1,42 +1,96 @@
 #!/bin/sh
 
-CWD=`pwd`
+CORESTORAGESTATUS="/private/tmp/corestorage.txt"
+ENCRYPTSTATUS="/private/tmp/encrypt_status.txt"
+ENCRYPTDIRECTION="/private/tmp/encrypt_direction.txt"
 
+OS=`/usr/bin/sw_vers | grep ProductVersion | cut -c 17-20`
 CONTEXT=`diskutil cs list | grep -E "Encryption Context" | awk '{print $3}'`
 ENCRYPTION=`diskutil cs list | grep -E "Encryption Type" | awk '{print $3}'`
 CONVERTED=`diskutil cs list | grep "Size (Converted)" | awk '{print $5, $6}'`
 SIZE=`diskutil cs list | grep "Size (Total)" | awk '{print $5, $6}'`
 
-diskutil cs list >> $CWD/corestorage.txt
- if grep -iE 'Logical Volume Family' $CWD/corestorage.txt; then
-  if [ "$CONTEXT" = "Present" ]; then
-   if [ "$ENCRYPTION" = "AES-XTS" ]; then
-	diskutil cs list | grep -E "Conversion Status" | awk '{print $3}' >> $CWD/encrypt_status.txt
-		if grep -iE 'Complete' $CWD/encrypt_status.txt; then 
-		  echo "FileVault 2 Encryption Complete"
-		else
-		  if  grep -iE 'Converting' $CWD/encrypt_status.txt; then
-		   diskutil cs list | grep -E "Conversion Direction" | awk '{print $3}' >> $CWD/encrypt_direction.txt
-		    if grep -iE 'Forward' $CWD/encrypt_direction.txt; then
-		        echo "FileVault 2 Encryption Proceeding. $CONVERTED of $SIZE Remaining"
-		   else
-		        echo "FileVault 2 Encryption Status Unknown. Please check."
-	            fi
-                   fi
-	          fi
-   else
-    if [ "$ENCRYPTION" = "None" ]; then
-      if grep -iE 'Reverse' $CWD/encrypt_direction.txt; then
-      echo "FileVault 2 Decryption Proceeding. $CONVERTED of $SIZE Remaining"
-      fi
-    fi 
-   fi
-  fi
-else 
-  if grep -iE 'No CoreStorage' $CWD/corestorage.txt; then
-   echo "FileVault 2 Encryption Not Enabled"
-  fi
+# Checks to see if the OS on the Mac is 10.7 or not.
+# If it is not, the following message is displayed without quotes:
+# "FileVault 2 Encryption Not Available For This Version Of Mac OS X"
+
+if [ "$OS" != "10.7" ]; then
+  echo "FileVault 2 Encryption Not Available For This Version Of Mac OS X"
 fi
-rm $CWD/encrypt_direction.txt
-rm $CWD/encrypt_status.txt
-rm $CWD/corestorage.txt
+
+
+
+if [ "$OS" = "10.7" ]; then
+  diskutil cs list >> $CORESTORAGESTATUS
+  
+    # If the Mac is running 10.7, but not does not have
+    # any CoreStorage volumes, the following message is 
+    # displayed without quotes:
+    # "FileVault 2 Encryption Not Enabled"
+    
+    if grep -iE 'No CoreStorage' $CORESTORAGESTATUS; then
+       echo "FileVault 2 Encryption Not Enabled"
+    fi
+    
+    # If the Mac is running 10.7 and has CoreStorage volumes,
+    # the script then checks to see if the machine is encrypted,
+    # encrypting, or decrypting.
+    # 
+    # If encrypted, the following message is 
+    # displayed without quotes:
+    # "FileVault 2 Encryption Complete"
+    #
+    # If encrypting, the following message is 
+    # displayed without quotes:
+    # "FileVault 2 Encryption Proceeding."
+    # How much has been encrypted of of the total
+    # amount of space is also displayed. If the
+    # amount of encryption is for some reason not
+    # known, the following message is 
+    # displayed without quotes:
+    # "FileVault 2 Encryption Status Unknown. Please check."
+    #
+    # If decrypting, the following message is 
+    # displayed without quotes:
+    # "FileVault 2 Decryption Proceeding"
+    # How much has been decrypted of of the total
+    # amount of space is also displayed
+    #
+    # If fully decrypted, the following message is 
+    # displayed without quotes:
+    # "FileVault 2 Decryption Complete"
+    #
+
+
+    if grep -iE 'Logical Volume Family' $CORESTORAGESTATUS; then
+      if [ "$CONTEXT" = "Present" ]; then
+        if [ "$ENCRYPTION" = "AES-XTS" ]; then
+	      diskutil cs list | grep -E "Conversion Status" | awk '{print $3}' >> $ENCRYPTSTATUS
+		    if grep -iE 'Complete' $ENCRYPTSTATUS; then 
+		      echo "FileVault 2 Encryption Complete"
+            else
+		      if  grep -iE 'Converting' $ENCRYPTSTATUS; then
+		        diskutil cs list | grep -E "Conversion Direction" | awk '{print $3}' >> $ENCRYPTDIRECTION
+		          if grep -iE 'Forward' $ENCRYPTDIRECTION; then
+		            echo "FileVault 2 Encryption Proceeding. $CONVERTED of $SIZE Remaining"
+                  else
+		            echo "FileVault 2 Encryption Status Unknown. Please check."
+	              fi
+               fi
+             fi
+        else
+            if [ "$ENCRYPTION" = "None" ]; then
+              diskutil cs list | grep -E "Conversion Direction" | awk '{print $3}' >> $ENCRYPTDIRECTION
+                if grep -iE 'Backward' $ENCRYPTDIRECTION; then
+                  echo "FileVault 2 Decryption Proceeding. $CONVERTED of $SIZE Remaining"
+                elif grep -iE '-none-' $ENCRYPTDIRECTION; then
+                  echo "FileVault 2 Decryption Completed"
+                fi
+            fi 
+        fi
+      fi  
+fi
+fi
+rm $ENCRYPTDIRECTION
+rm $ENCRYPTSTATUS
+rm $CORESTORAGESTATUS
