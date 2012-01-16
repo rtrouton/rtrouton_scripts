00cdf17fbbb8a80295cb7da1891e6b41d79acb61
rtrouton <rtrouton@yahoo.com>
2012-01-16 14:58:50 -0500

    Update rtrouton_scripts/setup_server_logging_to_central_syslog_server/setup_server_logging_to_central_syslog_server.sh

--- rtrouton_scripts/setup_server_logging_to_central_syslog_server/setup_server_logging_to_central_syslog_server.sh
+++ rtrouton_scripts/setup_server_logging_to_central_syslog_server/setup_server_logging_to_central_syslog_server.sh
@@ -1,5 +1,8 @@
-#!/bin/sh# Add syslog server(s) to /etc/syslog.conf
-/bin/echo "*.* @syslogserver1.domain.com" >> /etc/syslog.conf 
+#!/bin/sh
+
+# Add syslog server(s) to /etc/syslog.conf
+
+/bin/echo "*.* @syslogserver1.domain.com" >> /etc/syslog.conf 
 /bin/echo "*.* @syslogserver2.domain.com" >> /etc/syslog.conf
 
 # Force the syslog process to reread /etc/syslog.conf
