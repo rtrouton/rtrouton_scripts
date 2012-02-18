1eb12d81b3a6bc54603c7a991bf8ff18719e9180
Rich Trouton <rtrouton@yahoo.com>
2012-02-18 18:03:35 -0500

    Updating the enable_java_web_plugins script for 10.8 compatibility
    The lion_java_enable_web_plugins script did not run correctly on the
    10.8 DP 1 build. I've updated it so that it works correctly on both
    10.7.x and 10.8.x. I've also renamed the script to
    enable_java_web_plugins.sh to make it more generic. Will update blog
    entry with new GitHub address:
    
    http://derflounder.wordpress.com/2011/11/17/updated-script-to-activate-t
    he-java-web-plug-ins-setting-in-10-7/

--- rtrouton_scripts/enable_java_web_plugins/enable_java_web_plugins.sh
+++ rtrouton_scripts/enable_java_web_plugins/enable_java_web_plugins.sh
@@ -3,18 +3,21 @@
 # Set the the "Enable applet plug-in and Web Start Applications" setting for Java in your Mac's default user template and for all existing users.
 # Code adapted from DeployStudio's rc130 ds_finalize script, from the section where DeployStudio is disabling the iCloud and gestures demos
 
+osversionlong=`sw_vers -productVersion`
+osvers=${osversionlong:3:1}
+
 
 # Get the system's UUID to set ByHost prefs
 MAC_UUID=$(system_profiler SPHardwareDataType | awk -F" " '/UUID/{print $3}')
 
-# Checks first to see if the Mac is running 10.7. If so, the script
+# Checks first to see if the Mac is running 10.7 or 10.8. If so, the script
 # checks the system default user template for the presence of 
 # the Library/Preferences and Library/Preferences/ByHost directories.
 # If the directories are not found, they are created and then the
 # "Enable applet plug-in and Web Start Applications" setting for Java
 # setting is enabled.
 
-if [ `sw_vers -productVersion | awk -F. '{ print $2 }'` -ge 7 ]
+if [[ ${osvers} -eq 7 || 8 ]];
 then
   for USER_TEMPLATE in "/System/Library/User Template"/*
   do
@@ -28,20 +31,21 @@ then
      fi
      if [ -d "${USER_TEMPLATE}"/Library/Preferences/ByHost ]
       then
-        defaults write "${USER_TEMPLATE}"/Library/Preferences/ByHost/com.apple.java.JavaPreferences.${MAC_UUID} '{ GeneralByTask = { Any = { PrefsVersion = 2; WebComponentsEnabled = true;};};}'
+        /usr/libexec/PlistBuddy -c "Delete :GeneralByTask:Any:WebComponentsEnabled" "${USER_TEMPLATE}"/Library/Preferences/ByHost/com.apple.java.JavaPreferences.${MAC_UUID}.plist
+	/usr/libexec/PlistBuddy -c "Add :GeneralByTask:Any:WebComponentsEnabled bool true" "${USER_TEMPLATE}"/Library/Preferences/ByHost/com.apple.java.JavaPreferences.${MAC_UUID}.plist
       fi
   done
 fi
 
 
-# Checks first to see if the Mac is running 10.7. If so, the script
+# Checks first to see if the Mac is running 10.7 or 10.8. If so, the script
 # checks the existing user folders in /Users for the presence of 
 # the Library/Preferences and Library/Preferences/ByHost directories.
 # If the directories are not found, they are created and then the
 # "Enable applet plug-in and Web Start Applications" setting for Java
 # setting is enabled.
 
-if [ `sw_vers -productVersion | awk -F. '{ print $2 }'` -ge 7 ]
+if [[ ${osvers} -eq 7 || 8 ]];
 then
   for USER_HOME in /Users/*
   do
@@ -63,7 +67,8 @@ then
       fi
       if [ -d "${USER_HOME}"/Library/Preferences/ByHost ]
       then
-        defaults write "${USER_HOME}"/Library/Preferences/ByHost/com.apple.java.JavaPreferences.${MAC_UUID} '{ GeneralByTask = { Any = { PrefsVersion = 2; WebComponentsEnabled = true;};};}'
+         /usr/libexec/PlistBuddy -c "Delete :GeneralByTask:Any:WebComponentsEnabled" "${USER_HOME}"/Library/Preferences/ByHost/com.apple.java.JavaPreferences.${MAC_UUID}.plist
+	 /usr/libexec/PlistBuddy -c "Add :GeneralByTask:Any:WebComponentsEnabled bool true" "${USER_HOME}"/Library/Preferences/ByHost/com.apple.java.JavaPreferences.${MAC_UUID}.plist
         chown "${USER_UID}" "${USER_HOME}"/Library/Preferences/ByHost/com.apple.java.JavaPreferences.${MAC_UUID}.*
       fi
     fi
