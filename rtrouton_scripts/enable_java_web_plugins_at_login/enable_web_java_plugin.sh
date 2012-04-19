6ebd21c9e43c779daa032d06c65fb361d3f3cdf8
Rich Trouton <rtrouton@yahoo.com>
2012-04-18 20:18:08 -0400

    The GeneralByTask:Any:WebComponentsLastUsed key needs to be a dynamic date value rather than a static value that will expire.
     Joel Bruner in the comments pointed out that the real value associated
    with the GeneralByTask:Any:WebComponentsLastUsed key needs to be a
    dynamic date value rather than a static value that will expire. I’m
    incorporating his date calculating method into the script:
    
    http://derflounder.wordpress.com/2012/04/13/automatically-activate-the-j
    ava-web-plug-ins-setting-on-login/#comment-943

--- rtrouton_scripts/enable_java_web_plugins_at_login/enable_web_java_plugin.sh
+++ rtrouton_scripts/enable_java_web_plugins_at_login/enable_web_java_plugin.sh
@@ -13,4 +13,4 @@ fi
 /usr/libexec/PlistBuddy -c "Delete :GeneralByTask:Any:WebComponentsEnabled" /Users/$USER/Library/Preferences/ByHost/com.apple.java.JavaPreferences.${MAC_UUID}.plist
 /usr/libexec/PlistBuddy -c "Add :GeneralByTask:Any:WebComponentsEnabled bool true" /Users/$USER/Library/Preferences/ByHost/com.apple.java.JavaPreferences.${MAC_UUID}.plist
 /usr/libexec/PlistBuddy -c "Delete :GeneralByTask:Any:WebComponentsLastUsed" /Users/$USER/Library/Preferences/ByHost/com.apple.java.JavaPreferences.${MAC_UUID}.plist
-/usr/libexec/PlistBuddy -c "Add :GeneralByTask:Any:WebComponentsLastUsed real '356031204.300292'" /Users/$USER/Library/Preferences/ByHost/com.apple.java.JavaPreferences.${MAC_UUID}.plist
+/usr/libexec/PlistBuddy -c "Add :GeneralByTask:Any:WebComponentsLastUsed real $(( $(date "+%s") - 978307200 ))" /Users/$USER/Library/Preferences/ByHost/com.apple.java.JavaPreferences.${MAC_UUID}.plist
