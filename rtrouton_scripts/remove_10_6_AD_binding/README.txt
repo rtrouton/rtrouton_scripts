a824fdfae743b1aa1465d4f7de091eebad408156
Rich Trouton <rtrouton@yahoo.com>
2012-04-20 08:08:51 -0400

    Adding script to remove AD binding on 10.6.x Macs
    Script removes all Active Directory binding and AD-related Kerberos
    information on 10.6.x Macs
    
    Source:
    http://groups.google.com/group/macenterprise/msg/c635ab7bfc2b71d9?dmode=
    source

---
+++ rtrouton_scripts/remove_10_6_AD_binding/README.txt
@@ -0,0 +1,4 @@
+This script removes all Active Directory binding and AD-related Kerberos information on 10.6.x Macs
+
+Original script written by Ben LeRoy
+Source: http://groups.google.com/group/macenterprise/msg/c635ab7bfc2b71d9?dmode=source
---
+++ rtrouton_scripts/remove_10_6_AD_binding/remove_10_6_AD_Binding.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# This script removes all Active Directory binding
+# and AD-related Kerberos information on 10.6.x Macs
+#
+# Original script written by Ben LeRoy
+# Source: http://groups.google.com/group/macenterprise/msg/c635ab7bfc2b71d9?dmode=source
+# 
+
+rm /Library/Preferences/DirectoryService/ActiveDirectory.plist
+rm /Library/Preferences/DirectoryService/ActiveDirectoryDomainCache.plist
+rm /Library/Preferences/DirectoryService/ActiveDirectoryDomainPolicies.plist
+rm /Library/Preferences/DirectoryService/ActiveDirectoryDynamicData.plist
+dscl /Search -delete / CSPSearchPath /Active\ Directory/All\ Domains
+dscl /Search -delete /Search/Contacts CSPSearchPath /Active\ Directory/All\ Domains
+rm /Library/Preferences/edu.mit.Kerberos*
+rm /var/db/dslocal/nodes/Default/config/Kerberos\:*
+rm /var/db/dslocal/nodes/Default/config/AD\ DS\ Plugin.plist
+killall DirectoryService
