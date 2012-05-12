fafcf5acc541b015e1f79ae0b25c13221bcc7448
Rich Trouton <rtrouton@yahoo.com>
2012-05-12 12:42:00 -0400

    Posting Casper Extension Attribute to check the machine password interval on 10.5.x and 10.6.x Macs
    This Casper Extension Attribute checks 10.5 and 10.6 Macs to see if
    their Active Directory machine password interval is set to 0. If it's
    set to 0, that means that the machine password will never update.

---
+++ rtrouton_scripts/Casper_Extension_Attributes/check_10_5_and_10_6_AD_password_interval/README.txt
@@ -0,0 +1,3 @@
+This Casper Extension Attribute checks 10.5 and 10.6 Macs to see if their Active Directory machine password interval is set to 0. If it's set to 0, that means that the machine password will never update.
+
+See "Casper_Extension_Attribute_Setup.png" for a screenshot of how the Extension Attribute should be configured.
---
+++ rtrouton_scripts/Casper_Extension_Attributes/check_10_5_and_10_6_AD_password_interval/check_10_5_and_10_6_AD_password_interval.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+osvers=$(sw_vers -productVersion | awk -F. '{print $2}')
+
+if [[ ${osvers} -lt 5 ]]; then
+  echo '<result>'Unaffected'<result>'
+fi
+
+if [[ ${osvers} -ge 7 ]]; then
+  echo '<result>'Unaffected'<result>'
+fi
+ 
+# Checks AD password interval on 10.5.x Macs
+
+if [[ ${osvers} -eq 5 ]]; then
+   passinterval=`/usr/libexec/PlistBuddy -c "Print ':AD Advanced Options:Password Change Interval'" /Library/Preferences/DirectoryService/ActiveDirectory.plist`
+   if [ $passinterval -eq 0 ]; then
+      result=Yes
+   else
+      result=No
+   fi
+   echo "<result>$result</result>"
+fi
+
+# Checks AD password interval on 10.6.x Macs
+
+if [[ ${osvers} -eq 6 ]]; then
+   passinterval=`/usr/libexec/PlistBuddy -c "Print ':AD Advanced Options:Password Change Interval'" /Library/Preferences/DirectoryService/ActiveDirectory.plist`
+   if [ $passinterval -eq 0 ]; then
+      result=Yes
+   else
+      result=No
+   fi
+   echo "<result>$result</result>"
+fi
