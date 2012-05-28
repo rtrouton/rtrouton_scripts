9aed9b774af1ca96351570871455655ade2033a7
Rich Trouton <rtrouton@yahoo.com>
2012-05-28 07:19:42 -0400

    Script to ping machine then SSH in once machine is up.
    This script will check via ping to see if a machine is up. Once up, it
    will SSH into the machine using the IP / DNS address and username
    provided by the script's user.

---
+++ rtrouton_scripts/ssh_on_successful_ping/README.txt
@@ -0,0 +1,3 @@
+This script will check via ping to see if a machine is up. Once up, it will SSH into the machine using the IP / DNS address and username provided by the script's user.
+
+Inspiration: http://www.thegeekstuff.com/2010/06/bash-for-while-until-loop-examples/
---
+++ rtrouton_scripts/ssh_on_successful_ping/ssh_on_successful_ping.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+# At the prompt, enter the IP address
+# or DNS name of the machine you want
+# to connect to.
+
+read -p "Enter IP Address or Domain Name: " ipaddress
+echo $ipaddress
+
+# At the prompt, enter the username
+# of the account you want to log in
+# with.
+
+read -p "Enter Username: " sshusername
+echo $sshusername
+
+# The machine will be pinged
+# once per minute to check 
+# for a response. The until
+# loop statement ends only
+# when the ping returns a
+# successful response..
+
+until ping -c 1 $ipaddress
+do
+        sleep 60;
+done
+
+# Once ping returns successfully,
+# an SSH connection is attempted
+# using the username and IP address
+# or DNS name provided earlier.
+
+ssh $sshusername@$ipaddress
