e38043659ed1230789e7fb83bd0517a1716cdadf
Rich Trouton <rtrouton@yahoo.com>
2012-07-03 10:56:44 -0400

    Added Thunderbolt and USB ethernet settings to search domain setup.
    Added the following to the 10.7 firstboot script's search domain setup:
    
    /usr/sbin/networksetup -setsearchdomains "Thunderbolt Ethernet"
    $SearchDomains
    /usr/sbin/networksetup -setsearchdomains "USB Ethernet" $SearchDomains

--- rtrouton_scripts/first_boot/10.7/first_boot.sh
+++ rtrouton_scripts/first_boot/10.7/first_boot.sh
@@ -39,6 +39,8 @@ SearchDomains="searchdomain1.com searchdomain2.com searchdomain3.com"
 /usr/sbin/networksetup -setsearchdomains "Ethernet" $SearchDomains
 /usr/sbin/networksetup -setsearchdomains "Ethernet 1" $SearchDomains
 /usr/sbin/networksetup -setsearchdomains "Ethernet 2" $SearchDomains
+/usr/sbin/networksetup -setsearchdomains "Thunderbolt Ethernet" $SearchDomains
+/usr/sbin/networksetup -setsearchdomains "USB Ethernet" $SearchDomains
 
 # Enable secure virtual memory                                  
 #
