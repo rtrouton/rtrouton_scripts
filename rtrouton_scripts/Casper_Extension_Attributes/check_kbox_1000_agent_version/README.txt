8ca78f259c053ce7c5acb8a64478f39a195db09f
Rich Trouton <rtrouton@yahoo.com>
2012-11-12 09:27:09 -0500

    Posting Casper extension attribute to report on KBox 1000 agent version
    Posting a Casper Extension Attribute which checks to see if the Dell
    KACE KBox 1000 Mac agent is installed. If installed, the agent version
    is returned. If the agent is not installed, the script returns "Not
    Installed".

---
+++ rtrouton_scripts/Casper_Extension_Attributes/check_kbox_1000_agent_version/README.txt
@@ -0,0 +1,3 @@
+This Casper Extension Attribute checks to see if the Dell KACE KBox 1000 Mac agent is installed. If installed, the agent version is returned. If the agent is not installed, the script returns "Not Installed".
+
+See "Casper_Extension_Attribute_Setup.png" for a screenshot of how the Extension Attribute should be configured.
---
+++ rtrouton_scripts/Casper_Extension_Attributes/check_kbox_1000_agent_version/check_kbox_1000_agent_version.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Check to see if the KBox agent is installed.
+# If the agent is installed, report the agent
+# version.
+
+if [ -f "/Library/Application Support/Dell/KACE/data/version" ]; then
+   result=`cat "/Library/Application Support/Dell/KACE/data/version" | grep 5`
+   echo "<result>$result</result>"
+else
+   echo "<result>Not installed</result>"
+fi
