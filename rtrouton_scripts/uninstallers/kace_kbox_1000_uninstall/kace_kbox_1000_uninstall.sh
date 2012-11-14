c0b5fcb7ddbee5afd3253bba82d5c1cdecfda385
Rich Trouton <rtrouton@yahoo.com>
2012-11-14 09:31:07 -0500

    Removing sudo calls from KBox agent uninstall script
    Removing sudo calls from KBox agent uninstall script and adding /bin
    directory path to rm commands.

--- rtrouton_scripts/uninstallers/kace_kbox_1000_uninstall/kace_kbox_1000_uninstall.sh
+++ rtrouton_scripts/uninstallers/kace_kbox_1000_uninstall/kace_kbox_1000_uninstall.sh
@@ -2,26 +2,26 @@
  
 # Removes the 5.3 KBox agent
 
-sudo /Library/StartupItems/AMPAgentBootup/AMPAgentBootup stop
-sudo /Library/Application\ Support/Dell/KACE/bin/AMPctl stop 2>&1
-sudo /Library/Application\ Support/Dell/KACE/bin/kagentctl stop 2>&1
-sudo rm -rf /Library/Application\ Support/Dell
-sudo rm -rf /Library/StartupItems/AMPAgentBootup
-sudo rm -rf /Library/Receipts/AMPAgent.pkg
-sudo rm -rf /var/db/receipts/com.kace.ampagent.bom
-sudo rm -rf /var/db/receipts/com.kace.ampagent.plist
-sudo rm -rf /Library/LaunchAgents/com.kace.AdminAlert.plist
-sudo rm -rf /Library/LaunchDaemons/com.kace.ampagent.plist
+/Library/StartupItems/AMPAgentBootup/AMPAgentBootup stop
+/Library/Application\ Support/Dell/KACE/bin/AMPctl stop 2>&1
+/Library/Application\ Support/Dell/KACE/bin/kagentctl stop 2>&1
+/bin/rm -rf /Library/Application\ Support/Dell
+/bin/rm -rf /Library/StartupItems/AMPAgentBootup
+/bin/rm -rf /Library/Receipts/AMPAgent.pkg
+/bin/rm -rf /var/db/receipts/com.kace.ampagent.bom
+/bin/rm -rf /var/db/receipts/com.kace.ampagent.plist
+/bin/rm -rf /Library/LaunchAgents/com.kace.AdminAlert.plist
+/bin/rm -rf /Library/LaunchDaemons/com.kace.ampagent.plist
 
 # Removes the 5.1 KBox agent
 
-sudo /Library/StartupItems/KBOXAgent/KBOXAgent stop
-sudo /Library/KBOXAgent/Home/bin/kagentctl stop 2>&1
-sudo /Library/KBOXAgent/Home/bin/SMMPctl stop 2>&1
-sudo rm -rf /Library/KBOXAgent
-sudo rm -rf /Library/StartupItems/KBOXAgent
-sudo rm -rf /Library/Receipts/KBOX\ Agent.pkg
-sudo rm -rf /var/kace
-sudo rm -rf /var/db/receipts/com.kace.kboxagent.bom
-sudo rm -rf /var/db/receipts/com.kace.kboxagent.plist
-sudo rm -rf /Library/LaunchDaemons/kace.smmpagent.bootup.plist
+/Library/StartupItems/KBOXAgent/KBOXAgent stop
+/Library/KBOXAgent/Home/bin/kagentctl stop 2>&1
+/Library/KBOXAgent/Home/bin/SMMPctl stop 2>&1
+/bin/rm -rf /Library/KBOXAgent
+/bin/rm -rf /Library/StartupItems/KBOXAgent
+/bin/rm -rf /Library/Receipts/KBOX\ Agent.pkg
+/bin/rm -rf /var/kace
+/bin/rm -rf /var/db/receipts/com.kace.kboxagent.bom
+/bin/rm -rf /var/db/receipts/com.kace.kboxagent.plist
+/bin/rm -rf /Library/LaunchDaemons/kace.smmpagent.bootup.plist
