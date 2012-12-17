ea40e6486da07e8b1eb942ccd8cbb55e19270493
Rich Trouton <rtrouton@yahoo.com>
2012-12-17 15:15:43 -0500

    Posting Casper Extension Attribute that reports on the existence of Boot Camp partitions
    This script uses the diskutil list command to check for disk partitions
    reporting as "Microsoft Basic Data".
    
    If a Microsoft Basic Data partition is reported by diskutil, the script
    reports "Yes". If there is not a Microsoft Basic Data partition on the
    Mac, the script reports "No".

---
+++ rtrouton_scripts/Casper_Extension_Attributes/detect_bootcamp_partition/README.txt
@@ -0,0 +1,5 @@
+This Casper Extension Attribute uses the diskutil list command to check for disk partitions reporting as "Microsoft Basic Data".
+
+If Microsoft Basic Data partition is reported by diskutil, script reports "Yes". If no Microsoft Basic Data partition is reported by diskutil, script reports "No".
+
+See "Casper_Extension_Attribute_Setup.png" for a screenshot of how the Extension Attribute should be configured.
---
+++ rtrouton_scripts/Casper_Extension_Attributes/detect_bootcamp_partition/detect_bootcamp_partition.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+#
+# Using diskutil list to check for 
+# disk partitions reporting as 
+# "Microsoft Basic Data"
+#
+
+BOOTCAMP_DETECT=$( /usr/sbin/diskutil list | grep -c "Microsoft Basic Data" )
+
+#
+# If Microsoft Basic Data partition is
+# reported by diskutil, script reports
+# "Yes". If no Microsoft Basic Data partition
+# is reported by diskutil, script reports "No".
+# 
+
+if [[ "${BOOTCAMP_DETECT}" == "1" ]]; then
+      result=Yes
+   else
+      result=No
+   fi
+echo "<result>$result</result>"
+
+exit 0
