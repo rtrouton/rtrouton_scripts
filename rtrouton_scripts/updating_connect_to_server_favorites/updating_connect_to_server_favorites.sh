9ba3f21d1ca081f8fbef98fb2c9a6169c08e35ab
Rich Trouton <rtrouton@yahoo.com>
2013-01-16 09:57:36 -0500

    Updated the updating_connect_to_server_favorites script
    I made some updates to the updating_connect_to_server_favorites script
    to include OS and error checking. It now checks to see if it's running
    on 10.6.x and higher before running the updates, and also backs up the
    existing file.

--- rtrouton_scripts/updating_connect_to_server_favorites/updating_connect_to_server_favorites.sh
+++ rtrouton_scripts/updating_connect_to_server_favorites/updating_connect_to_server_favorites.sh
@@ -1,14 +1,46 @@
 #!/bin/sh
 
-# Convert plist to XML
+# Determine OS version
+osvers=$(sw_vers -productVersion | awk -F. '{print $2}')
 
-/usr/bin/plutil -convert xml1 /Users/username/Library/Preferences/com.apple.sidebarlists.plist
+# Get current date
+FILE_DATE=`date +%Y%m%d`
 
-# Search and replace in /Users/username/Library/Preferences/com.apple.sidebarlists.plist
 
-/usr/bin/sed -i "" -e 's/smb:\/\/oldservername\/oldsharename/smb:\/\/newservername\/newsharename/g' /Users/username/Library/Preferences/com.apple.sidebarlists.plist
+# If the Mac is running 10.5.8 or lower, the script should exit
 
-# Fix permissions on file
+if [[ ${osvers} -lt 6 ]]; then
+   exit 0
+fi
 
-/usr/sbin/chown username /Users/username/Library/Preferences/com.apple.sidebarlists.plist
+# If the Mac is running 10.6.0 or higher, the script should should run
 
+if [[ ${osvers} -ge 6 ]]; then
+   
+   # Check for the /Users/username/Library/Preferences/com.apple.sidebarlists.plist file
+
+   if [ -f /Users/username/Library/Preferences/com.apple.sidebarlists.plist ]; then
+      
+      # Back up the existing file
+      
+      /bin/cp /Users/username/Library/Preferences/com.apple.sidebarlists.plist /Users/username/Library/Preferences/com.apple.sidebarlists-$FILE_DATE.plist
+
+      # Fix permissions on backup file
+
+      /usr/sbin/chown username /Users/username/Library/Preferences/com.apple.sidebarlists-$FILE_DATE.plist
+      
+      # Convert plist to XML
+
+      /usr/bin/plutil -convert xml1 /Users/username/Library/Preferences/com.apple.sidebarlists.plist
+
+      # Search and replace in /Users/username/Library/Preferences/com.apple.sidebarlists.plist
+      /usr/bin/sed -i "" -e 's/smb:\/\/oldservername\/oldsharename/smb:\/\/newservername\/newsharename/g' /Users/username/Library/Preferences/com.apple.sidebarlists.plist
+
+      # Fix permissions on file
+      
+      /usr/sbin/chown username /Users/username/Library/Preferences/com.apple.sidebarlists.plist
+
+   fi
+fi
+
+exit 0
