c81a5d26ae8266e171d7439482f2d7942ccc84ce
Rich Trouton <rtrouton@yahoo.com>
2013-01-22 11:20:16 -0500

    Updating AD bind checking extension attribute
    Updating AD bind checking extension attribute for Casper. The previous
    version was providing more false positives than actual unbinds, so I've
    added additional checking.

--- rtrouton_scripts/Casper_Extension_Attributes/check_ad_binding/check_ad_binding.sh
+++ rtrouton_scripts/Casper_Extension_Attributes/check_ad_binding/check_ad_binding.sh
@@ -3,10 +3,25 @@
 username=AD_account_here
 
 id $username
+
+if [ $? -ne 0 ]; then
+   sleep 120
+
+   id $username
+   if [ $? -ne 0 ]; then
+      result=No
+      echo "<result>$result</result>"
+   fi
+
+   id $username
+   if [ $? -eq 0 ]; then
+      result=Yes
+      echo "<result>$result</result>"
+   fi
+  exit 0
+fi
+
 if [ $? -eq 0 ]; then
       result=Yes
-else
-      result=No
+      echo "<result>$result</result>"
 fi
-echo "<result>$result</result>"
-
