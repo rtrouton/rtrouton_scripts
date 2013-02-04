09326898068738cb40fc7d460b1ac000e9ad40f5
Rich Trouton <rtrouton@yahoo.com>
2013-02-04 11:54:13 -0500

    Caught a problem with grep -c output, fixing with grep -o
    Caught a problem with grep -c output giving false positives, fixing
    with grep -o.

--- rtrouton_scripts/install_apple_java_updates/install_apple_java_updates.sh
+++ rtrouton_scripts/install_apple_java_updates/install_apple_java_updates.sh
@@ -7,7 +7,7 @@
 # as an available update.
 #
 
-JAVA_UPDATE_DETECT=$( softwareupdate -l | grep -c "JavaForMacOSX10.6-12.0" )
+JAVA_UPDATE_DETECT=$( softwareupdate -l | grep -o "JavaForMacOSX10.6-12.0" )
 
 #
 # If Java for Mac OS X 10.6 Update 12
@@ -17,12 +17,11 @@ JAVA_UPDATE_DETECT=$( softwareupdate -l | grep -c "JavaForMacOSX10.6-12.0" )
 # exits.
 # 
 
-if [[ "${JAVA_UPDATE_DETECT}" == "1" ]]; then
+if [[ "${JAVA_UPDATE_DETECT}" = "JavaForMacOSX10.6-12.0" ]]; then
       logger "Installing Java for Mac OS X 10.6 Update 12"
       softwareupdate --install JavaForMacOSX10.6-12.0
    else
-      logger "Java for Mac OS X 10.6 Update 12 not an available update. Exiting"
+      logger "Java for Mac OS X 10.6 Update 12 not an available update. Exiting."
 fi
 
 exit 0
-
