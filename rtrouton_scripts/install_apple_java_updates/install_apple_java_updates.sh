3a144de531667dd9a6ecb15b7eb00905f6a6110e
Rich Trouton <rtrouton@yahoo.com>
2013-03-04 17:08:37 -0500

    Updated script for Java for Mac OS X 10.6 Update 14
    Made changes to script and README to reflect Apple's recent update for
    Java 6, from Java for Mac OS X 10.6 Update 13 to Java for Mac OS X 10.6
    Update 14.

--- rtrouton_scripts/install_apple_java_updates/install_apple_java_updates.sh
+++ rtrouton_scripts/install_apple_java_updates/install_apple_java_updates.sh
@@ -3,25 +3,25 @@
 #
 # Using the softwareupdate tool
 # to detect if the Mac has
-# Java for Mac OS X 10.6 Update 13
+# Java for Mac OS X 10.6 Update 14
 # as an available update.
 #
 
-JAVA_UPDATE_DETECT=$( softwareupdate -l | grep -o "JavaForMacOSX10.6-13.0" )
+JAVA_UPDATE_DETECT=$( softwareupdate -l | grep -o "JavaForMacOSX10.6-14.0" )
 
 #
-# If Java for Mac OS X 10.6 Update 13
+# If Java for Mac OS X 10.6 Update 14
 # is an available update, script installs 
-# the update. If Java for Mac OS X 10.6 Update 13 is
+# the update. If Java for Mac OS X 10.6 Update 14 is
 # not an available update, script reports that and
 # exits.
 # 
 
-if [[ "${JAVA_UPDATE_DETECT}" = "JavaForMacOSX10.6-13.0" ]]; then
-      logger "Installing Java for Mac OS X 10.6 Update 13"
-      softwareupdate --install JavaForMacOSX10.6-13.0
+if [[ "${JAVA_UPDATE_DETECT}" = "JavaForMacOSX10.6-14.0" ]]; then
+      logger "Installing Java for Mac OS X 10.6 Update 14"
+      softwareupdate --install JavaForMacOSX10.6-14.0
    else
-      logger "Java for Mac OS X 10.6 Update 13 not an available update. Exiting."
+      logger "Java for Mac OS X 10.6 Update 14 not an available update. Exiting."
 fi
 
 exit 0
