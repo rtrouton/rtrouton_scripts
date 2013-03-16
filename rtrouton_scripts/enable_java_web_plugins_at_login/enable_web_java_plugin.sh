9baeba7a6954708ecc2a213fb915b44ded3f6459
Rich Trouton <rtrouton@yahoo.com>
2013-03-15 20:58:36 -0400

    Updating the enable_web_java_plugin script for Safari 6.0.3 and later.
    One of the features of Apple’s Safari 6.0.3 update is that it turns off
    the automatic execution of Java applets through Safari, even if the
    Java browser plug-in is otherwise enabled.
    
    Safari 6.0.3 does allow for the automatic execution of Java applets to
    be re-enabled through the browser. However, if it’s been a while since
    a Java applet was launched, then automatic execution of Java applets is
    once again automatically disabled.
    
    I've updated my existing enable_web_java_plugin script to also handle
    re-enabling Java browser plug-ins in Safari.

--- rtrouton_scripts/enable_java_web_plugins_at_login/enable_web_java_plugin.sh
+++ rtrouton_scripts/enable_java_web_plugins_at_login/enable_web_java_plugin.sh
@@ -7,6 +7,14 @@ elif [[ `ioreg -rd1 -c IOPlatformExpertDevice | grep -i "UUID" | cut -c27-50` !=
 	MAC_UUID=`ioreg -rd1 -c IOPlatformExpertDevice | grep -i "UUID" | cut -c27-62`
 fi
 
+# Enable Java browser plug-ins in Safari 6.0.3 and later
+# for the current user by setting the com.apple.WebKit.JavaPlugInLastUsedTimestamp
+# key in ~/Library/Preferences/.GlobalPreferences.plist
+
+/usr/libexec/PlistBuddy -c "Delete :com.apple.WebKit.JavaPlugInLastUsedTimestamp" $HOME/Library/Preferences/.GlobalPreferences.plist
+/usr/libexec/PlistBuddy -c "Add :com.apple.WebKit.JavaPlugInLastUsedTimestamp real $(( $(date "+%s") - 978307200 ))" $HOME/Library/Preferences/.GlobalPreferences.plist
+/usr/bin/plutil -convert xml1 $HOME/Library/Preferences/.GlobalPreferences.plist
+
 # Set the the "Enable applet plug-in and Web Start Applications" setting in
 # the Java Preferences for the current user.
 
@@ -14,3 +22,8 @@ fi
 /usr/libexec/PlistBuddy -c "Add :GeneralByTask:Any:WebComponentsEnabled bool true" $HOME/Library/Preferences/ByHost/com.apple.java.JavaPreferences.${MAC_UUID}.plist
 /usr/libexec/PlistBuddy -c "Delete :GeneralByTask:Any:WebComponentsLastUsed" $HOME/Library/Preferences/ByHost/com.apple.java.JavaPreferences.${MAC_UUID}.plist
 /usr/libexec/PlistBuddy -c "Add :GeneralByTask:Any:WebComponentsLastUsed real $(( $(date "+%s") - 978307200 ))" $HOME/Library/Preferences/ByHost/com.apple.java.JavaPreferences.${MAC_UUID}.plist
+/usr/bin/plutil -convert xml1 $HOME/Library/Preferences/ByHost/com.apple.java.JavaPreferences.${MAC_UUID}.plist
+
+# Forces preferences to be re-read
+
+/usr/bin/killall cfprefsd
