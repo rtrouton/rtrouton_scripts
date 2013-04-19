591ff35398cdc4472bf063269fbc5506a7f7d618
Rich Trouton <rtrouton@yahoo.com>
2013-04-19 07:29:51 -0400

    Fixing spacing in safari_set_java_whitelist_at_login.sh
    Some extra tabbing showed up in safari_set_java_whitelist_at_login when
    it wasn't wanted. Spacing is now consistently set.

--- rtrouton_scripts/safari_java_whitelist/safari_set_java_whitelist_at_login/safari_set_java_whitelist_at_login.sh
+++ rtrouton_scripts/safari_java_whitelist/safari_set_java_whitelist_at_login/safari_set_java_whitelist_at_login.sh
@@ -19,20 +19,20 @@ if [[ ${osvers} -ge 6 ]]; then
   if [[ -n ${SERVER1_WHITELIST_CHECK} ]]; then
 
         # Server1 settings are present
-	    /usr/bin/logger "${SERVER1_WHITELIST_CHECK} is part of the Java whitelist in Safari. Nothing to do here."
+	/usr/bin/logger "${SERVER1_WHITELIST_CHECK} is part of the Java whitelist in Safari. Nothing to do here."
     else	    
-		# Add Server1 to Java whitelist
+	# Add Server1 to Java whitelist
         /usr/bin/defaults write $HOME/Library/Preferences/com.apple.Safari "WhitelistedBlockedPlugins" -array-add '{"PluginHostname" = "server1.name.here"; "PluginIdentifier" = "'$JAVA_PLUGIN'"; "PluginLastVisitedDate" = "'$TODAY'"; "PluginName" = "Java Applet Plug-in"; "PluginPageURL" = "https://server1.name.here"; "PluginPolicy" = "PluginPolicyNeverBlock";}'
         /usr/bin/logger "server1.name.here has been added to the Java whitelist in Safari."
   fi
 
   if [[ -n ${SERVER2_WHITELIST_CHECK} ]]; then
 
-		# Server2 settings are present
-		/usr/bin/logger "${SERVER2_WHITELIST_CHECK} is part of the Java whitelist in Safari. Nothing to do here."
+	# Server2 settings are present
+	/usr/bin/logger "${SERVER2_WHITELIST_CHECK} is part of the Java whitelist in Safari. Nothing to do here."
      else		
         # Add Server2 to Java whitelist
-		/usr/bin/defaults write $HOME/Library/Preferences/com.apple.Safari "WhitelistedBlockedPlugins" -array-add '{"PluginHostname" = "server2.name.here"; "PluginIdentifier" = "'$JAVA_PLUGIN'"; "PluginLastVisitedDate" = "'$TODAY'"; "PluginName" = "Java Applet Plug-in"; "PluginPageURL" = "https://server2.name.here"; "PluginPolicy" = "PluginPolicyNeverBlock";}'
+	/usr/bin/defaults write $HOME/Library/Preferences/com.apple.Safari "WhitelistedBlockedPlugins" -array-add '{"PluginHostname" = "server2.name.here"; "PluginIdentifier" = "'$JAVA_PLUGIN'"; "PluginLastVisitedDate" = "'$TODAY'"; "PluginName" = "Java Applet Plug-in"; "PluginPageURL" = "https://server2.name.here"; "PluginPolicy" = "PluginPolicyNeverBlock";}'
         /usr/bin/logger "server2.name.here has been added to the Java whitelist in Safari."
   fi
 
