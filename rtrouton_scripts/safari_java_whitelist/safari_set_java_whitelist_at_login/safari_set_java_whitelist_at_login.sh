1c429451a83ea466430582e63641a79bc64bfff6
Rich Trouton <rtrouton@yahoo.com>
2013-04-27 21:31:36 -0400

    Adding a SERVER1 and SERVER2 variable to the safari_set_java_whitelist_at_login.sh
    Adding a SERVER1 and SERVER2 variable to the
    safari_set_java_whitelist_at_login.sh. This allows the server addresses
    to be set once and used several places in the script.

--- rtrouton_scripts/safari_java_whitelist/safari_set_java_whitelist_at_login/safari_set_java_whitelist_at_login.sh
+++ rtrouton_scripts/safari_java_whitelist/safari_set_java_whitelist_at_login/safari_set_java_whitelist_at_login.sh
@@ -6,14 +6,20 @@ TODAY=$(/bin/date "+%FT%TZ")
 # Determine OS version
 osvers=$(sw_vers -productVersion | awk -F. '{print $2}')
 
+# Server1's address
+SERVER1=server1.name.here
+
+# Server2's address
+SERVER2=server2.name.here
+
 # Get Java plug-in info
 JAVA_PLUGIN=`/usr/bin/defaults read "/Library/Internet Plug-Ins/JavaAppletPlugin.plugin/Contents/Info" CFBundleIdentifier`
 
 # Check com.apple.Safari.plist for Server1 address
-SERVER1_WHITELIST_CHECK=`/usr/bin/defaults read $HOME/Library/Preferences/com.apple.Safari WhitelistedBlockedPlugins | grep PluginHostname | awk '{print $3}' | grep server1.name.here | tr -d '";'`
+SERVER1_WHITELIST_CHECK=`/usr/bin/defaults read $HOME/Library/Preferences/com.apple.Safari WhitelistedBlockedPlugins | grep PluginHostname | awk '{print $3}' | grep $SERVER1 | tr -d '";'`
 
 # Check com.apple.Safari.plist for Server2 address
-SERVER2_WHITELIST_CHECK=`/usr/bin/defaults read $HOME/Library/Preferences/com.apple.Safari WhitelistedBlockedPlugins | grep PluginHostname | awk '{print $3}' | grep server2.name.here | tr -d '";'`
+SERVER2_WHITELIST_CHECK=`/usr/bin/defaults read $HOME/Library/Preferences/com.apple.Safari WhitelistedBlockedPlugins | grep PluginHostname | awk '{print $3}' | grep $SERVER2 | tr -d '";'`
 
 if [[ ${osvers} -ge 6 ]]; then
   if [[ -n ${SERVER1_WHITELIST_CHECK} ]]; then
@@ -22,8 +28,8 @@ if [[ ${osvers} -ge 6 ]]; then
 	/usr/bin/logger "${SERVER1_WHITELIST_CHECK} is part of the Java whitelist in Safari. Nothing to do here."
     else	    
 	# Add Server1 to Java whitelist
-        /usr/bin/defaults write $HOME/Library/Preferences/com.apple.Safari "WhitelistedBlockedPlugins" -array-add '{"PluginHostname" = "server1.name.here"; "PluginIdentifier" = "'$JAVA_PLUGIN'"; "PluginLastVisitedDate" = "'$TODAY'"; "PluginName" = "Java Applet Plug-in"; "PluginPageURL" = "https://server1.name.here"; "PluginPolicy" = "PluginPolicyNeverBlock";}'
-        /usr/bin/logger "server1.name.here has been added to the Java whitelist in Safari."
+        /usr/bin/defaults write $HOME/Library/Preferences/com.apple.Safari "WhitelistedBlockedPlugins" -array-add '{"PluginHostname" = "'$SERVER1'"; "PluginIdentifier" = "'$JAVA_PLUGIN'"; "PluginLastVisitedDate" = "'$TODAY'"; "PluginName" = "Java Applet Plug-in"; "PluginPageURL" = "https://'$SERVER1'"; "PluginPolicy" = "PluginPolicyNeverBlock";}'
+        /usr/bin/logger "$SERVER1 has been added to the Java whitelist in Safari."
   fi
 
   if [[ -n ${SERVER2_WHITELIST_CHECK} ]]; then
@@ -32,8 +38,8 @@ if [[ ${osvers} -ge 6 ]]; then
 	/usr/bin/logger "${SERVER2_WHITELIST_CHECK} is part of the Java whitelist in Safari. Nothing to do here."
      else		
         # Add Server2 to Java whitelist
-	/usr/bin/defaults write $HOME/Library/Preferences/com.apple.Safari "WhitelistedBlockedPlugins" -array-add '{"PluginHostname" = "server2.name.here"; "PluginIdentifier" = "'$JAVA_PLUGIN'"; "PluginLastVisitedDate" = "'$TODAY'"; "PluginName" = "Java Applet Plug-in"; "PluginPageURL" = "https://server2.name.here"; "PluginPolicy" = "PluginPolicyNeverBlock";}'
-        /usr/bin/logger "server2.name.here has been added to the Java whitelist in Safari."
+	/usr/bin/defaults write $HOME/Library/Preferences/com.apple.Safari "WhitelistedBlockedPlugins" -array-add '{"PluginHostname" = "'$SERVER2'"; "PluginIdentifier" = "'$JAVA_PLUGIN'"; "PluginLastVisitedDate" = "'$TODAY'"; "PluginName" = "Java Applet Plug-in"; "PluginPageURL" = "https://'$SERVER2'"; "PluginPolicy" = "PluginPolicyNeverBlock";}'
+        /usr/bin/logger "$SERVER2 has been added to the Java whitelist in Safari."
   fi
 
 fi
