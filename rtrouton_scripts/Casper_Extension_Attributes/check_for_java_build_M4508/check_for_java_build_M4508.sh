21a3da6324d9245c04e3ff23dd9024cd3a4a47ab
Rich Trouton <rtrouton@yahoo.com>
2013-06-24 10:01:04 -0400

    Comment fixing in check_for_java_build_M4508.sh
    Caught a couple of grammer mistakes in my comments. Now fixed.

--- rtrouton_scripts/Casper_Extension_Attributes/check_for_java_build_M4508/check_for_java_build_M4508.sh
+++ rtrouton_scripts/Casper_Extension_Attributes/check_for_java_build_M4508/check_for_java_build_M4508.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 # Determines if either of the following 
-# Java build are installed: 
+# Java builds are installed: 
 #
 # 10.6.x: 1.6.0_51-b11-456-10M4508
 # 10.7.x - 10.8.x: 1.6.0_51-b11-456-11M4508
 #
-# This build was installed by the first versions of
+# This builds were installed by the first versions of
 # Java for OS X 2013-004 and Java for Mac OS X 10.6 Update 16 
 # and can cause problems for Java Swing applications
 # like MATLAB and Papercut.
