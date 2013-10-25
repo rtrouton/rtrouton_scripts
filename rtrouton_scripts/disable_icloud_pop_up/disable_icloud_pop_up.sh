4f8111a58d2e8faec3056c8fd1dc652652c80a89
Rich Trouton <rtrouton@yahoo.com>
2013-10-25 17:53:12 -0400

    Posting updated disable_icloud_pop_up script
    I updated my disable_icloud_pop_up script so that it now also stops the
    iCloud pop-ups on 10.9. Tested on 10.8.5 and 10.9.0.

--- rtrouton_scripts/disable_icloud_pop_up/disable_icloud_pop_up.sh
+++ rtrouton_scripts/disable_icloud_pop_up/disable_icloud_pop_up.sh
@@ -2,6 +2,8 @@
 
 # Determine OS version
 osvers=$(sw_vers -productVersion | awk -F. '{print $2}')
+sw_vers=$(sw_vers -productVersion)
+sw_build=$(sw_vers -buildVersion)
 
 # Checks first to see if the Mac is running 10.7.0 or higher. 
 # If so, the script checks the system default user template
@@ -16,10 +18,19 @@ if [[ ${osvers} -ge 7 ]]; then
   do
     defaults write "${USER_TEMPLATE}"/Library/Preferences/com.apple.SetupAssistant DidSeeCloudSetup -bool TRUE
     defaults write "${USER_TEMPLATE}"/Library/Preferences/com.apple.SetupAssistant GestureMovieSeen none
-    defaults write "${USER_TEMPLATE}"/Library/Preferences/com.apple.SetupAssistant LastSeenCloudProductVersion 10.8
+    defaults write "${USER_TEMPLATE}"/Library/Preferences/com.apple.SetupAssistant LastSeenCloudProductVersion "${sw_vers}"
   done
 
- # Checks first to see if the Mac is running 10.6 or higher.
+if [[ ${osvers} -ge 9 ]]; then
+
+ for USER_TEMPLATE in "/System/Library/User Template"/*
+  do
+    defaults write "${USER_TEMPLATE}"/Library/Preferences/com.apple.SetupAssistant LastPreLoginTasksPerformedVersion "${sw_vers}"
+    defaults write "${USER_TEMPLATE}"/Library/Preferences/com.apple.SetupAssistant LastPreLoginTasksPerformedBuild "${sw_build}"
+  done
+fi
+
+ # Checks first to see if the Mac is running 10.7.0 or higher.
  # If so, the script checks the existing user folders in /Users
  # for the presence of the Library/Preferences directory.
  #
@@ -41,7 +52,11 @@ if [[ ${osvers} -ge 7 ]]; then
       then
         defaults write "${USER_HOME}"/Library/Preferences/com.apple.SetupAssistant DidSeeCloudSetup -bool TRUE
         defaults write "${USER_HOME}"/Library/Preferences/com.apple.SetupAssistant GestureMovieSeen none
-        defaults write "${USER_HOME}"/Library/Preferences/com.apple.SetupAssistant LastSeenCloudProductVersion 10.8
+        defaults write "${USER_HOME}"/Library/Preferences/com.apple.SetupAssistant LastSeenCloudProductVersion "${sw_vers}"
+        	if [[ ${osvers} -ge 9 ]]; then
+        		defaults write "${USER_HOME}"/Library/Preferences/com.apple.SetupAssistant LastPreLoginTasksPerformedVersion "${sw_vers}"
+        		defaults write "${USER_HOME}"/Library/Preferences/com.apple.SetupAssistant LastPreLoginTasksPerformedBuild "${sw_build}"
+        	fi
         chown "${USER_UID}" "${USER_HOME}"/Library/Preferences/com.apple.SetupAssistant.plist
       fi
     fi
