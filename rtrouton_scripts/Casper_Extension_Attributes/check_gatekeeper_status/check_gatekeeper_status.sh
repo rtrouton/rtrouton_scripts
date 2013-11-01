82042bebd838d38d7a145f47a56e173ac18fcef2
Rich Trouton <rtrouton@yahoo.com>
2013-11-01 08:44:00 -0400

    Updating Gatekeeper status check script and Casper Extension attribute
    Now that 10.9 is here, I've updated the Gatekeeper status check script
    and Casper Extension attribute to properly handle Gatekeeper status on
    10.7.x and higher.

--- rtrouton_scripts/Casper_Extension_Attributes/check_gatekeeper_status/check_gatekeeper_status.sh
+++ rtrouton_scripts/Casper_Extension_Attributes/check_gatekeeper_status/check_gatekeeper_status.sh
@@ -3,16 +3,13 @@
 osvers=$(sw_vers -productVersion | awk -F. '{print $2}')
 
 if [[ ${osvers} -lt 7 ]]; then
-  echo "<result>Gatekeeper Not Available For This Version Of Mac OS X</result>"
-fi
-
-if [[ ${osvers} -ge 9 ]]; then
-  echo "<result>Future Not Known Yet. Revise Me In Mid-2013</result>"
+  echo "Gatekeeper Not Available For This Version Of Mac OS X"
 fi
  
-# Checks Gatekeeper status on 10.7.x Macs
+# Checks Gatekeeper status on Macs
+# running 10.7.x or higher
 
-if [[ ${osvers} -eq 7 ]]; then
+if [[ ${osvers} -ge 7 ]]; then
     gatekeeper_status=`spctl --status | grep "assessments" | cut -c13-`
    if [ $gatekeeper_status = "disabled" ]; then
       result=Disabled
@@ -20,16 +17,4 @@ if [[ ${osvers} -eq 7 ]]; then
       result=Active
    fi
    echo "<result>$result</result>"
-fi
-
-# Checks Gatekeeper status on 10.8.x Macs
-
-if [[ ${osvers} -eq 8 ]]; then
-    gatekeeper_status=`spctl --status | grep "assessments" | cut -c13-`
-   if [ $gatekeeper_status = "disabled" ]; then
-      result=Disabled
-   else
-      result=Active
-   fi
-  echo "<result>$result</result>"
 fi
--- rtrouton_scripts/check_gatekeeper_status/check_gatekeeper_status.sh
+++ rtrouton_scripts/check_gatekeeper_status/check_gatekeeper_status.sh
@@ -5,26 +5,11 @@ osvers=$(sw_vers -productVersion | awk -F. '{print $2}')
 if [[ ${osvers} -lt 7 ]]; then
   echo "Gatekeeper Not Available For This Version Of Mac OS X"
 fi
-
-if [[ ${osvers} -ge 9 ]]; then
-  echo "Future Not Known Yet. Revise Me In Mid-2013"
-fi
  
-# Checks Gatekeeper status on 10.7.x Macs
+# Checks Gatekeeper status on Macs
+# running 10.7.x or higher
 
-if [[ ${osvers} -eq 7 ]]; then
-    gatekeeper_status=`spctl --status | grep "assessments" | cut -c13-`
-   if [ $gatekeeper_status = "disabled" ]; then
-      result=Disabled
-   else
-      result=Active
-   fi
-   echo $result
-fi
-
-# Checks Gatekeeper status on 10.8.x Macs
-
-if [[ ${osvers} -eq 8 ]]; then
+if [[ ${osvers} -ge 7 ]]; then
     gatekeeper_status=`spctl --status | grep "assessments" | cut -c13-`
    if [ $gatekeeper_status = "disabled" ]; then
       result=Disabled
