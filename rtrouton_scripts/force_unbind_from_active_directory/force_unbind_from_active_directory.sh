04bceb6a284fe728f3e73a97aba05c4f12b903e0
Rich Trouton <rtrouton@yahoo.com>
2013-11-26 22:14:59 -0500

    Updating force_unbind_from_active_directory.sh and its associated payload-free package
    Updating force_unbind_from_active_directory.sh and its associated
    payload-free package to better address Macs running 10.6.x and below.

--- rtrouton_scripts/force_unbind_from_active_directory/force_unbind_from_active_directory.sh
+++ rtrouton_scripts/force_unbind_from_active_directory/force_unbind_from_active_directory.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
-
 # Script to force AD unbinding
 #
 # Using bogus user since dsconfigad
 # wants a specified user account
- 
-dsconfigad -force -remove -u nousernamehere -p nopasswordhere
+
+# Determine OS version
+osvers=$(sw_vers -productVersion | awk -F. '{print $2}')
+
+if [[ ${osvers} -lt 7 ]]; then
+  dsconfigad -f -r -u nousernamehere -p nopasswordhere
+fi
+
+if [[ ${osvers} -ge 7 ]]; then
+ dsconfigad -force -remove -u nousernamehere -p nopasswordhere
+fi
