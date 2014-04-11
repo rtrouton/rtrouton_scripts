a85580853a6299af116f1ad71e2bd00fc18bd4bf
owenx <owenpragel@gmail.com>
2014-04-11 03:46:53 -0700

    Initial changes
    Updates to download Flash 13, should prevent Flash installer DMG from
    showing up in the root of the OS X boot drive.

--- rtrouton_scripts/install_latest_adobe_flash_player/install_latest_adobe_flash_player.sh
+++ rtrouton_scripts/install_latest_adobe_flash_player/install_latest_adobe_flash_player.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 # This script downloads and installs the latest Flash player for compatible Macs
 
 # Determine OS version
@@ -7,37 +6,26 @@ osvers=$(sw_vers -productVersion | awk -F. '{print $2}')
 
 if [[ ${osvers} -lt 6 ]]; then
   echo "Adobe Flash Player is not available for Mac OS X 10.5.8 or below."
+  exit 1
 fi
 
 if [[ ${osvers} -ge 6 ]]; then
- 
-	# Change working directory to /tmp
 
-	/usr/bin/cd /tmp
+  # Download the latest Adobe Flash Player software disk image
+  curl http://fpdownload.macromedia.com/get/flashplayer/current/licensing/mac/install_flash_player_13_osx_pkg.dmg -L -o /tmp/install_flash_player_13_osx_pkg.dmg
 
-	# Download the latest Adobe Flash Player software disk image
+  # Mount the install_flash_player_13_osx.dmg disk image in /Volumes
+  hdiutil mount -quiet -nobrowse /tmp/install_flash_player_13_osx_pkg.dmg
 
-	/usr/bin/curl -O http://fpdownload.macromedia.com/get/flashplayer/current/licensing/mac/install_flash_player_12_osx_pkg.dmg
+  # Install Adobe Flash Player from the installer package inside Install Adobe Flash Player.app
+  # Note: Installing Flash Player this way will not install the Adobe Flash Updater
+  installer -pkg "/Volumes/Flash Player/Install Adobe Flash Player.pkg" -target "/"
 
-	# Mount the install_flash_player_12_osx.dmg disk image in /Volumes
+  # Unmount the install_flash_player_13_osx.dmg disk image from   /Volumes
+  hdiutil unmount /Volumes/Flash\ Player
 
-	/usr/bin/hdiutil attach install_flash_player_12_osx_pkg.dmg -nobrowse -noverify -noautoopen
-
-	# Install Adobe Flash Player from the installer package inside Install Adobe Flash Player.app
-	# Note: Installing Flash Player this way will not install the Adobe Flash Updater
-
-	/usr/sbin/installer -dumplog -verbose -pkg "/Volumes/Flash Player/Install Adobe Flash Player.pkg" -target "/"
-
-	# Clean-up
- 
-	# Unmount the install_flash_player_11_osx.dmg disk image from /Volumes
-
-	/usr/bin/hdiutil eject -force /Volumes/Flash\ Player
-
-	# Remove the install_flash_player_11_osx.dmg disk image from /tmp
-
-	/bin/rm -rf /tmp/install_flash_player_12_osx_pkg.dmg
+  # Remove the install_flash_player_13_osx.dmg disk image from /tmp
+  rm /tmp/install_flash_player_13_osx_pkg.dmg
 
 fi
-
 exit 0
