7d47ca55ec25077e47e8e4e1f9ff57c226549885
owen-74bit <owen@74bit.com>
2014-06-16 01:12:58 -0700

    Update install_latest_adobe_flash_player.sh

--- rtrouton_scripts/install_latest_adobe_flash_player/install_latest_adobe_flash_player.sh
+++ rtrouton_scripts/install_latest_adobe_flash_player/install_latest_adobe_flash_player.sh
@@ -1,31 +1,54 @@
 #!/bin/sh
+
 # This script downloads and installs the latest Flash player for compatible Macs
 
 # Determine OS version
 osvers=$(sw_vers -productVersion | awk -F. '{print $2}')
 
+# Specify the complete address of the Adobe Flash Player
+# disk image
+ 
+fileURL="http://fpdownload.macromedia.com/get/flashplayer/current/licensing/mac/install_flash_player_14_osx_pkg.dmg"
+
+# Specify name of downloaded disk image
+
+flash_dmg="/tmp/flash.dmg"
+
 if [[ ${osvers} -lt 6 ]]; then
   echo "Adobe Flash Player is not available for Mac OS X 10.5.8 or below."
-  exit 1
 fi
 
 if [[ ${osvers} -ge 6 ]]; then
-
+ 
   # Download the latest Adobe Flash Player software disk image
-  curl http://fpdownload.macromedia.com/get/flashplayer/current/licensing/mac/install_flash_player_13_osx_pkg.dmg -L -o /tmp/install_flash_player_13_osx_pkg.dmg
 
-  # Mount the install_flash_player_13_osx.dmg disk image in /Volumes
-  hdiutil mount -quiet -nobrowse /tmp/install_flash_player_13_osx_pkg.dmg
+  /usr/bin/curl --output "$flash_dmg" "$fileURL"
 
-  # Install Adobe Flash Player from the installer package inside Install Adobe Flash Player.app
-  # Note: Installing Flash Player this way will not install the Adobe Flash Updater
-  installer -pkg "/Volumes/Flash Player/Install Adobe Flash Player.pkg" -target "/"
+    # Specify a /tmp/flashplayer.XXXX mountpoint for the disk image
+ 
+    TMPMOUNT=`/usr/bin/mktemp -d /tmp/flashplayer.XXXX`
 
-  # Unmount the install_flash_player_13_osx.dmg disk image from   /Volumes
-  hdiutil unmount /Volumes/Flash\ Player
+    # Mount the latest Flash Player disk image to /tmp/flashplayer.XXXX mountpoint
+ 
+    hdiutil attach "$flash_dmg" -mountpoint "$TMPMOUNT" -nobrowse -noverify -noautoopen
 
-  # Remove the install_flash_player_13_osx.dmg disk image from /tmp
-  rm /tmp/install_flash_player_13_osx_pkg.dmg
+  # Install Adobe Flash Player from the installer package stored inside the disk image
 
+    /usr/sbin/installer -dumplog -verbose -pkg "$(/usr/bin/find $TMPMOUNT -maxdepth 1 \( -iname \*\.pkg -o -iname \*\.mpkg \))" -target "/"
+
+  # Clean-up
+ 
+    # Unmount the Flash Player disk image from /tmp/flashplayer.XXXX
+ 
+    /usr/bin/hdiutil detach "$TMPMOUNT"
+ 
+    # Remove the /tmp/flashplayer.XXXX mountpoint
+ 
+    /bin/rm -rf "$TMPMOUNT"
+
+    # Remove the downloaded disk image
+
+    /bin/rm -rf "$flash_dmg"
 fi
+
 exit 0
