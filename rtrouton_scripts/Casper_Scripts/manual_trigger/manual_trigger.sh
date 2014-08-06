750a8e16fba99613e394e1685684f0337d889713
Rich Trouton <rtrouton@yahoo.com>
2014-08-06 08:05:13 -0400

    Adding script to manually trigger a Casper policy
    Script for use with Casper when manually triggering a policy. Script
    uses a manual trigger name (set as Parameter 4 in the script's
    parameter list in the JSS) to specify which policy should be run.

---
+++ rtrouton_scripts/Casper_Scripts/manual_trigger/manual_trigger.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This script runs a manual policy trigger to
+# allow the policy or policies associated with that
+# trigger to be executed.
+
+
+trigger_name="$4"
+
+/usr/sbin/jamf policy -trigger "$trigger_name"
+
+exit 0
