8c4802d8070c6a2614bbb743f9cc014d4dc4aa14
rtrouton <rtrouton@yahoo.com>
2014-09-03 22:18:22 -0400

    Update filevault_2_encryption_check_extension_attribute.sh
    Fixing </result></result> tags.

--- rtrouton_scripts/filevault_2_encryption_check/Casper Extension Attribute/filevault_2_encryption_check_extension_attribute.sh
+++ rtrouton_scripts/filevault_2_encryption_check/Casper Extension Attribute/filevault_2_encryption_check_extension_attribute.sh
@@ -167,7 +167,7 @@ fi
             fi  
        fi
       if [ "$ENCRYPTIONEXTENTS" = "No" ]; then
-		      echo "FileVault 2 Encryption Not Enabled"
+		      echo "<result>FileVault 2 Encryption Not Enabled</result>"
       fi
      fi
 
