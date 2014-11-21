550b69ce2de4e5d8bc4ce90a3c8491cfc1305c5e
Rich Trouton <rtrouton@yahoo.com>
2014-11-20 19:19:27 -0500

    Uploading setting_diagnostic_result_reporting scripts
    Uploading setting_diagnostic_result_reporting scripts and an associated
    payload-free package.

---
+++ rtrouton_scripts/setting_diagnostic_result_reporting/README.md
@@ -0,0 +1,27 @@
+This script sets whether you want to send diagnostic info from Yosemite and later back to Apple and/or third party app developers by setting the appropriate values in **/Library/Application Support/CrashReporter/DiagnosticMessagesHistory.plist**.
+
+If you want to send diagonostic data to Apple, set the following value in the script for the **SUBMIT_DIAGNOSTIC_DATA_TO_APPLE** value:
+
+`SUBMIT_DIAGNOSTIC_DATA_TO_APPLE=TRUE`
+
+If you want to send data to third party app developers, set the following value in the script for the **SUBMIT_DIAGNOSTIC_DATA_TO_APP_DEVELOPERS** value:
+
+`SUBMIT_DIAGNOSTIC_DATA_TO_APP_DEVELOPERS=TRUE`
+
+By default, the values in this script are set to send no diagnostic data, either to Apple or to third party app developers:
+
+`SUBMIT_DIAGNOSTIC_DATA_TO_APPLE=FALSE` 
+
+`SUBMIT_DIAGNOSTIC_DATA_TO_APP_DEVELOPERS=FALSE`
+
+To change this in your own script, comment out the **FALSE** lines and uncomment the **TRUE** lines as appropriate.
+  
+**Installer package**
+
+I've built a payload-free installer package which is available for download from the **payload-free** directory. This installer package runs the script using the following values:
+
+`SUBMIT_DIAGNOSTIC_DATA_TO_APPLE=FALSE` 
+
+`SUBMIT_DIAGNOSTIC_DATA_TO_APP_DEVELOPERS=FALSE`
+
+These settings will set the Mac in question to send no diagnostic data, either to Apple or to third party app developers.
---
+++ rtrouton_scripts/setting_diagnostic_result_reporting/setting_diagnostic_result_reporting.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Determine OS version
+osvers=$(sw_vers -productVersion | awk -F. '{print $2}')
+
+# Set whether you want to send diagnostic info back to
+# Apple and/or third party app developers. If you want
+# to send diagonostic data to Apple, set the following 
+# value for the SUBMIT_DIAGNOSTIC_DATA_TO_APPLE value:
+#
+# SUBMIT_DIAGNOSTIC_DATA_TO_APPLE=TRUE
+# 
+# If you want to send data to third party app developers,
+# set the following value for the
+# SUBMIT_DIAGNOSTIC_DATA_TO_APP_DEVELOPERS value:
+#
+# SUBMIT_DIAGNOSTIC_DATA_TO_APP_DEVELOPERS=TRUE
+# 
+# By default, the values in this script are set to 
+# send no diagnostic data: 
+
+SUBMIT_DIAGNOSTIC_DATA_TO_APPLE=FALSE
+SUBMIT_DIAGNOSTIC_DATA_TO_APP_DEVELOPERS=FALSE
+
+# To change this in your own script, comment out the FALSE
+# lines and uncomment the TRUE lines as appropriate.
+
+# Checks first to see if the Mac is running 10.10.0 or higher. 
+# If so, the desired diagnostic submission settings are applied.
+
+if [[ ${osvers} -ge 10 ]]; then
+
+  CRASHREPORTER_SUPPORT="/Library/Application Support/CrashReporter"
+ 
+  if [ ! -d "${CRASHREPORTER_SUPPORT}" ]; then
+    mkdir "${CRASHREPORTER_SUPPORT}"
+    chmod 775 "${CRASHREPORTER_SUPPORT}"
+    chown root:admin "${CRASHREPORTER_SUPPORT}"
+  fi
+
+ /usr/bin/defaults write "$CRASHREPORTER_SUPPORT"/DiagnosticMessagesHistory AutoSubmit -boolean ${SUBMIT_DIAGNOSTIC_DATA_TO_APPLE}
+ /usr/bin/defaults write "$CRASHREPORTER_SUPPORT"/DiagnosticMessagesHistory AutoSubmitVersion -int 4
+ /usr/bin/defaults write "$CRASHREPORTER_SUPPORT"/DiagnosticMessagesHistory ThirdPartyDataSubmit -boolean ${SUBMIT_DIAGNOSTIC_DATA_TO_APP_DEVELOPERS}
+ /usr/bin/defaults write "$CRASHREPORTER_SUPPORT"/DiagnosticMessagesHistory ThirdPartyDataSubmitVersion -int 4
+ /bin/chmod a+r "$CRASHREPORTER_SUPPORT"/DiagnosticMessagesHistory.plist
+ /usr/sbin/chown root:admin "$CRASHREPORTER_SUPPORT"/DiagnosticMessagesHistory.plist
+
+fi
+
+exit 0
