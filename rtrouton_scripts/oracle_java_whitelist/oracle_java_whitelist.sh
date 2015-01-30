7a2f4e38b31a0dc5e1b5cbc94488247e8e992b03
jacobsalmela <jacob.salmela@gmail.com>
2015-01-30 10:05:00 -0600

    for loop for unlimited servers
    Using a for loop to give the capability to add as many servers as
    needed without creating additional if statements.

--- rtrouton_scripts/oracle_java_whitelist/oracle_java_whitelist.sh
+++ rtrouton_scripts/oracle_java_whitelist/oracle_java_whitelist.sh
@@ -6,67 +6,41 @@
 # empty (i.e. SERVER2='') as the script will do a check to see if either SERVER value
 # is set to be null.
 
-# Server1's address
-SERVER1='http://server.name.here'
-
-# Server2's address
-SERVER2='https://server.name.here'
+# Enter as many servers as needed -- one per line with single quotes
+SERVERS=('http://server.name.here'
+'https://server.name.here'
+'https://another.server.name.here'
+'https://as.many.as.needed')
 
 LOGGER="/usr/bin/logger"
 WHITELIST=$HOME"/Library/Application Support/Oracle/Java/Deployment/security/exception.sites"
-SERVER1_WHITELIST_CHECK=`cat $HOME"/Library/Application Support/Oracle/Java/Deployment/security/exception.sites" | grep $SERVER1`
-SERVER2_WHITELIST_CHECK=`cat $HOME"/Library/Application Support/Oracle/Java/Deployment/security/exception.sites" | grep $SERVER2`
+JAVA_PLUGIN=$(/usr/bin/defaults read "/Library/Internet Plug-Ins/JavaAppletPlugin.plugin/Contents/Info" CFBundleIdentifier)
 
-JAVA_PLUGIN=`/usr/bin/defaults read "/Library/Internet Plug-Ins/JavaAppletPlugin.plugin/Contents/Info" CFBundleIdentifier`
 
 if [[ ${JAVA_PLUGIN} != 'com.oracle.java.JavaAppletPlugin' ]]; then
-   ${LOGGER} "Oracle Java browser plug-in not installed"
-   exit 0
+	${LOGGER} "Oracle Java browser plug-in not installed"
+	exit 1
+else
+	${LOGGER} "Oracle Java browser plug-in IS installed."
+	if [[ ! -f "$WHITELIST" ]]; then
+		${LOGGER} "Oracle Java Exception Site List not found. Creating one..."
+		touch  "$WHITELIST"
+ 
+		for i in "${SERVERS[@]}"
+		do
+			${LOGGER} "Adding exception for: $i..."
+			echo $i >> "$WHITELIST"
+		done
+	else
+		for i in "${SERVERS[@]}"
+		do
+			WHITELIST_CHECK=$(cat $HOME"/Library/Application Support/Oracle/Java/Deployment/security/exception.sites" | grep $i)
+			if [[ -n ${WHITELIST_CHECK} ]];then
+				${LOGGER} "Exception already exists..."
+			else
+				${LOGGER} "Adding exception for: $i..."
+				echo "$i" >> "$WHITELIST"
+			fi
+		done
+	fi
 fi
-
-if [[ ${JAVA_PLUGIN} = 'com.oracle.java.JavaAppletPlugin' ]]; then
- ${LOGGER} "Oracle Java browser plug-in is installed. Checking for Exception Site List."
- if [[ ! -f "$WHITELIST" ]]; then
-   ${LOGGER} "Oracle Java Exception Site List not found. Creating Exception Site List."
-
-   # Create exception.sites file
-   touch  "$WHITELIST"
-
-   # Add needed server(s) to exception.sites file
-   if [[ -n ${SERVER1} ]]; then 
-     /bin/echo "$SERVER1" >> "$WHITELIST"
-   fi
-   if [[ -n ${SERVER2} ]]; then
-     /bin/echo "$SERVER2" >> "$WHITELIST"
-   fi
-   exit 0
- fi
-
- if [[ -f "$WHITELIST" ]]; then
-   ${LOGGER} "Oracle Java Exception Site List Found."
-
-  if [[ -n ${SERVER1_WHITELIST_CHECK} ]]; then
-
-    # Server1 settings are present
-	${LOGGER} "${SERVER1_WHITELIST_CHECK} is part of the Oracle Java Exception Site List. Nothing to do here."
-    else	    
-	# Add Server1 to exception.sites file
-    if [[ -n ${SERVER1} ]]; then 
-      /bin/echo "$SERVER1" >> "$WHITELIST"
-      ${LOGGER} "$SERVER1 has been added to the Oracle Java Exception Site List."
-    fi
-  fi
-  if [[ -n ${SERVER2_WHITELIST_CHECK} ]]; then
-
-    # Server2 settings are present
-	${LOGGER} "${SERVER2_WHITELIST_CHECK} is part of the Oracle Java Exception Site List. Nothing to do here."
-    else	    
-	# Add Server2 to exception.sites file
-    if [[ -n ${SERVER2} ]]; then 
-      /bin/echo "$SERVER2" >> "$WHITELIST"
-      ${LOGGER} "$SERVER2 has been added to the Oracle Java Exception Site List."
-    fi  
-   fi
- fi
-fi
-exit 0
