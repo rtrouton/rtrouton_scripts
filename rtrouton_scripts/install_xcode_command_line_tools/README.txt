fb26cb1440d93583d0d0f4c94fc2e11f0354e7b1
Rich Trouton <rtrouton@yahoo.com>
2015-02-02 13:24:41 -0500

    Uploading the install_xcode_command_line_tools script
    Uploading a script to install the Xcode command line tools on 10.7.x
    through 10.10.x.

---
+++ rtrouton_scripts/install_xcode_command_line_tools/README.txt
@@ -0,0 +1,29 @@
+This script will download and install the Xcode command line tools on Macs running 10.7.x and higher. 
+
+How the script works:
+
+On 10.9.x and 10.10.x:
+
+1. Creates a placeholder file in /tmp. This file's existence is checked by the softwareupdate tool before allowing the installation of the Xcode command line tools.
+
+2. Runs the softwareupdate tool and checks for the latest version of the Xcode command line tools for the OS in question.
+
+3. Uses the softwareupdate tool to install the latest version of the Xcode command line tools for the OS in question.
+
+
+On 10.7.x and 10.8.x:
+
+1. Uses curl to download a disk image containing the specified Xcode Command Line Tools installer from Apple's web site
+
+2. Renames the downloaded disk image to clitools.dmg.
+
+2. Mounts the disk image silently in /tmp. Disk image will not be visible to any logged-in user.
+
+3. Installs the Xcode Command Line Tools using the installer package stored on the disk image
+
+4. After installation, unmounts the disk image and removes it from the Mac in question.
+
+
+Note: This script should not be used in combination with a payload-free installer package. On 10.9.x and 10.10.x, the softwareupdate tool will not work properly when called from within a payload-free package.
+
+
---
+++ rtrouton_scripts/install_xcode_command_line_tools/install_xcode_command_line_tools.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# Installing the Xcode command line tools on 10.7.x through 10.10.x
+
+osx_vers=$(sw_vers -productVersion | awk -F "." '{print $2}')
+cmd_line_tools_temp_file="/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress"
+
+# Installing the latest Xcode command line tools on 10.9.x or 10.10.x
+
+if [[ "$osx_vers" -eq 9 ]] || [[ "$osx_vers" -eq 10 ]]; then
+
+	# Create the placeholder file which is checked by the softwareupdate tool 
+	# before allowing the installation of the Xcode command line tools.
+	
+	touch "$cmd_line_tools_temp_file"
+	
+	# Find the last listed update in the Software Update feed with "Command Line Tools" in the name
+	
+	cmd_line_tools=$(softwareupdate -l | awk '/\*\ Command Line Tools/ { $1=$1;print }' | tail -1 | sed 's/^[[ \t]]*//;s/[[ \t]]*$//;s/*//' | cut -c 2-)
+	
+	#Install the command line tools
+	
+	softwareupdate -i "$cmd_line_tools" -v
+	
+	# Remove the temp file
+	
+	if [[ -f "cmd_line_tools_temp_file" ]]; then
+	  rm "$cmd_line_tools_temp_file"
+	fi
+fi
+
+# Installing the latest Xcode command line tools on 10.7.x and 10.8.x
+
+# on 10.7/10.8, instead of using the software update feed, the command line tools are downloaded
+# instead from public download URLs, which can be found in the dvtdownloadableindex:
+# https://devimages.apple.com.edgekey.net/downloads/xcode/simulators/index-3905972D-B609-49CE-8D06-51ADC78E07BC.dvtdownloadableindex
+
+if [[ "$osx_vers" -eq 7 ]] || [[ "$osx_vers" -eq 8 ]]; then
+
+	if [[ "$osx_vers" -eq 7 ]]; then
+	    DMGURL=http://devimages.apple.com/downloads/xcode/command_line_tools_for_xcode_os_x_lion_april_2013.dmg
+	fi
+	
+	if [[ "$osx_vers" -eq 8 ]]; then
+	     DMGURL=http://devimages.apple.com/downloads/xcode/command_line_tools_for_xcode_os_x_mountain_lion_march_2014.dmg
+	fi
+
+		TOOLS=clitools.dmg
+		curl "$DMGURL" -o "$TOOLS"
+		TMPMOUNT=`/usr/bin/mktemp -d /tmp/clitools.XXXX`
+		hdiutil attach "$TOOLS" -mountpoint "$TMPMOUNT" -nobrowse
+		installer -pkg "$(find $TMPMOUNT -name '*.mpkg')" -target /
+		hdiutil detach "$TMPMOUNT"
+		rm -rf "$TMPMOUNT"
+		rm "$TOOLS"
+fi
+
+exit 0
