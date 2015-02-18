41ff3b1e7b76605fdadf568e5aa9d82cbe5a19d3
Rich Trouton <rtrouton@yahoo.com>
2015-02-18 09:32:49 -0500

    Updating check_ad_binding Extension Attribute
    To hopefully cut down on false positives, I’ve updated my
    check_ad_binding Extension Attribute.

--- rtrouton_scripts/Casper_Extension_Attributes/check_ad_binding/check_ad_binding.sh
+++ rtrouton_scripts/Casper_Extension_Attributes/check_ad_binding/check_ad_binding.sh
@@ -1,27 +1,40 @@
 #!/bin/bash
 
-username=AD_account_here
+username="username_goes_here"
+ad_domain="domain_name_goes_here"
+check_domain=$( dsconfigad -show | awk '/Active Directory Domain/{print $NF}' )
 
-id $username
-
-if [ $? -ne 0 ]; then
-   sleep 120
-
-   id $username
-   if [ $? -ne 0 ]; then
-      result=No
-      echo "<result>$result</result>"
-   fi
-
-   id $username
-   if [ $? -eq 0 ]; then
-      result=Yes
-      echo "<result>$result</result>"
-   fi
-  exit 0
+if [[ "$check_domain" != "$ad_domain" ]]; then
+  result="NA"
+  echo "<result>$result</result>"
 fi
 
-if [ $? -eq 0 ]; then
+if [[ "$check_domain" == "$ad_domain" ]]; then
+
+  id $username
+
+  if [ $? -ne 0 ]; then
+     sleep 240
+
+     id $username
+     if [ $? -ne 0 ]; then
+        result=No
+        echo "<result>$result</result>"
+     fi
+
+     id $username
+     if [ $? -eq 0 ]; then
+        result=Yes
+        echo "<result>$result</result>"
+     fi
+    exit 0
+  fi
+
+  if [ $? -eq 0 ]; then
       result=Yes
       echo "<result>$result</result>"
+  fi
+
 fi
+
+exit 0
