56e7fcaf1e749d8b75075d130d1b691196c7b969
Rich Trouton <rtrouton@yahoo.com>
2015-04-23 13:50:38 -0400

    Uploading install_joylent_pkgsrc script and payload-free package
    Uploading install_joylent_pkgsrc script and payload-free package. This
    script is designed to do the following:
    
    1. Uses curl to download an OS-appropriate gzipped tar file containing
    the latest pkgsrc bootstrap installer from http://pkgsrc.joyent.com.
    2. Renames the downloaded tar file to pkgsrc.tar.gz and stores it in
    /tmp.
    3. Installs pkgsrc into /usr/pkg using the pkgsrc bootstrap installer.
    4. Updates pkgsrc with the latest package info.
    5. After installation, removes the downloaded tar file from the Mac in
    question.

---
+++ rtrouton_scripts/install_joylent_pkgsrc/install_joylent_pkgsrc.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+osvers_major=$(sw_vers -productVersion | awk -F. '{print $1}')
+osvers_minor=$(sw_vers -productVersion | awk -F. '{print $2}')
+
+# Function to provide custom curl options
+myCurl () { /usr/bin/curl -k --retry 3 --silent --show-error "$@"; }
+
+# For the SixtyFourBitURL variable, put the complete address of the 64-bit pkgsrc bootstrap kit.
+# This variable's URL may change periodically. Check the following website for the latest address:
+# http://pkgsrc.joyent.com/install-on-osx/
+
+SixtyFourBitURL="http://pkgsrc.joyent.com/packages/Darwin/bootstrap/bootstrap-2015Q1-x86_64.tar.gz"
+
+# For the ThirtyTwoBitURL variable, put the complete address of the 32-bit pkgsrc bootstrap kit.
+# This variable's URL may change periodically. Check the following website for the latest address:
+# http://pkgsrc.joyent.com/install-on-osx/
+
+ThirtyTwoBitURL="http://pkgsrc.joyent.com/packages/Darwin/bootstrap/bootstrap-2015Q1-i386.tar.gz"
+
+# Specify name and location of the downloaded pkgsrc bootstrap kit
+
+bootstrap_kit="/tmp/pkgsrc.tar.gz"
+
+# Checks to see if the OS on the Mac is 10.x.x. If it is not, the 
+# following message is displayed without quotes:
+#
+# "Unknown Version Of Mac OS X"
+
+if [[ ${osvers_major} -ne 10 ]]; then
+  echo "Unknown Version Of Mac OS X"
+  exit 0
+fi
+
+# Checks to see if the OS on the Mac is earlier than Mac OS X 10.6.0.
+# If it is, the following message is displayed without quotes:
+#
+# "pkgsrc is not available for this version of Mac OS X"
+
+if [[ ${osvers_major} -eq 10 ]] && [[ ${osvers_minor} -lt 6 ]]; then
+  echo "pkgsrc is not available for this version of Mac OS X"
+  exit 0
+fi
+
+# Checks to see if the OS on the Mac is 10.8.x or earlier. 
+# If it is, the 32-bit pkgsrc bootstrap kit is downloaded.
+
+if [[ ${osvers_major} -eq 10 ]] && [[ ${osvers_minor} -lt 9 ]]; then
+  myCurl --output "$bootstrap_kit" "$ThirtyTwoBitURL"
+fi
+
+# Checks to see if the OS on the Mac is 10.9.x or later. 
+# If it is, the 64-bit pkgsrc bootstrap kit is downloaded.
+
+if [[ ${osvers_major} -eq 10 ]] && [[ ${osvers_minor} -ge 9 ]]; then
+  myCurl --output "$bootstrap_kit" "$SixtyFourBitURL"
+fi
+
+# Install pkgsrc bootstrap kit to /usr/pkg
+
+if [[ -f "$bootstrap_kit" ]]; then
+  /usr/bin/tar -zxpf "$bootstrap_kit" -C /
+else
+  echo "$bootstrap_kit not found."
+fi
+
+# Fetch the latest package repository information for pkgsrc
+
+if [[ -d "/usr/pkg" ]]; then
+  /usr/pkg/bin/pkgin -y update
+fi
+
+# Clean up by removing the downloaded pkgsrc bootstrap kit
+
+if [[ -e "$bootstrap_kit" ]]; then
+  rm -rf "$bootstrap_kit"
+fi
+
+exit 0
