e29234ff00ef7e16c9bada52e237a4b667546f31
Rich Trouton <rtrouton@yahoo.com>
2015-05-26 17:57:36 -0400

    Updating enable_external_network_adapter.sh to now accommodate all laptops.
    Updating enable_external_network_adapter.sh to now accommodate all
    laptops. Listing individual laptop models was getting unwieldy.

--- rtrouton_scripts/enable_external_network_adapter/enable_external_network_adapter.sh
+++ rtrouton_scripts/enable_external_network_adapter/enable_external_network_adapter.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
-# Checks to see if the Mac is either a MacBook Pro Retina or MacBook Air
-# If it's either of these machines, the script will then check for external USB
-# or Thunderbolt network adapters. If either adapter is present, it will add the 
+# Checks to see if the Mac is either a MacBook, MacBook Pro Retina or MacBook Air.
+# If it's any of these machines, the script will then check for external USB
+# or Thunderbolt network adapters. If an adapter is present, it will add the 
 # adapter to network services.
 #
 # Resolves an issue with USB & Thunderbolt Ethernet adapters with DeployStudio
@@ -10,34 +10,28 @@
 # Original script by Allen Golbig:
 # https://github.com/golbiga/Scripts/tree/master/enable_external_network_adapter
 
-
-mbpr=`system_profiler SPHardwareDataType | awk '/Model Identifier/{print $3}' | cut -f1 -d ","`
-mba=`system_profiler SPHardwareDataType | awk '/Model Identifier/{print $3}' | cut -c-10`
+macbook_check=`/usr/sbin/system_profiler SPHardwareDataType | awk '/Model Name/' | awk -F': ' '{print substr($2,1,7)}'`
 usbAdapter=`/usr/sbin/networksetup -listallhardwareports | grep "Hardware Port: USB Ethernet"`
+usbGigAdapter=`/usr/sbin/networksetup -listallhardwareports | grep "Hardware Port: USB Gigabit Ethernet"`
 tbAdapter=`/usr/sbin/networksetup -listallhardwareports | grep "Hardware Port: Thunderbolt Ethernet"`
 
 /usr/sbin/networksetup -detectnewhardware
 
-if [[ $mbpr = "MacBookPro10" ]] || [[ $mbpr = "MacBookPro11" ]] || [[ $mbpr = "MacBookPro12" ]]; then
+if [ "$macbook_check" = "MacBook" ]; then
 	if [ "$usbAdapter" != "" ]; then
 		/usr/sbin/networksetup -createnetworkservice USB\ Ethernet 'USB Ethernet'
 		echo "USB Ethernet added to Network Services"
 	else
 		echo "No USB Adapter connected"
 	fi
-	if [ "$tbAdapter" != "" ]; then
-		/usr/sbin/networksetup -createnetworkservice Thunderbolt\ Ethernet 'Thunderbolt Ethernet'
-		echo "Thunderbolt Ethernet added to Network Services"
+
+	if [ "$usbGigAdapter" != "" ]; then
+		/usr/sbin/networksetup -createnetworkservice USB\ Gigabit\ Ethernet 'USB Gigabit Ethernet'
+		echo "USB Gigabit Ethernet Adapter added to Network Services"
 	else
-		echo "No Thunderbolt Adapter connected"
-	fi	
-elif [ $mba = "MacBookAir" ]; then
-	if [ "$usbAdapter" != "" ]; then
-		/usr/sbin/networksetup -createnetworkservice USB\ Ethernet 'USB Ethernet'
-		echo "USB Ethernet added to Network Services"
-	else
-		echo "No USB Adapter connected"
+		echo "No USB Gigabit Ethernet Adapter connected"
 	fi
+
 	if [ "$tbAdapter" != "" ]; then
 		/usr/sbin/networksetup -createnetworkservice Thunderbolt\ Ethernet 'Thunderbolt Ethernet'
 		echo "Thunderbolt Ethernet added to Network Services"
