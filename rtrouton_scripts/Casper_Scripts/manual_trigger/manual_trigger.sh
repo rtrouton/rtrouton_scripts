d44c21401c2fc094d3f883b0d1619bce2d0913ff
Rich Trouton <rtrouton@yahoo.com>
2015-10-21 07:43:57 -0400

    Updating Casper manual trigger script to detect location of the jamf binary
    Updating Casper manual trigger script to detect location of the jamf
    binary, using the CheckBinary function from CasperCheck.

--- rtrouton_scripts/Casper_Scripts/manual_trigger/manual_trigger.sh
+++ rtrouton_scripts/Casper_Scripts/manual_trigger/manual_trigger.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
 
 # This script runs a manual policy trigger to
-# allow the policy or policies associated with that
+# allow the policie(s) associated with that
 # trigger to be executed.
-
 
 trigger_name="$4"
 
-jamf policy -trigger "$trigger_name"
+CheckBinary (){
+
+# Identify location of jamf binary.
+
+jamf_binary=`/usr/bin/which jamf`
+
+ if [[ "$jamf_binary" == "" ]] && [[ -e "/usr/sbin/jamf" ]] && [[ ! -e "/usr/local/bin/jamf" ]]; then
+    jamf_binary="/usr/sbin/jamf"
+ elif [[ "$jamf_binary" == "" ]] && [[ ! -e "/usr/sbin/jamf" ]] && [[ -e "/usr/local/bin/jamf" ]]; then
+    jamf_binary="/usr/local/bin/jamf"
+ elif [[ "$jamf_binary" == "" ]] && [[ -e "/usr/sbin/jamf" ]] && [[ -e "/usr/local/bin/jamf" ]]; then
+    jamf_binary="/usr/local/bin/jamf"
+ fi
+}
+
+# Run the CheckBinary function to identify the location
+# of the jamf binary for the jamf_binary variable.
+
+CheckBinary
+
+$jamf_binary policy -trigger "$trigger_name"
 
 exit 0
