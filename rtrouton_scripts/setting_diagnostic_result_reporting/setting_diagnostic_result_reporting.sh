d387db77bc1e4145e09bad8f275fce9df8e74372
Rich Trouton <rtrouton@yahoo.com>
2016-01-21 08:09:08 -0500

    Updating the setting_diagnostic_result_reporting for El Capitan.
    Updating the setting_diagnostic_result_reporting for El Capitan.
    AutoSubmitVersion and ThirdPartyDataSubmitVersion need to use a value
    of 5, rather than 4.

--- rtrouton_scripts/setting_diagnostic_result_reporting/setting_diagnostic_result_reporting.sh
+++ rtrouton_scripts/setting_diagnostic_result_reporting/setting_diagnostic_result_reporting.sh
@@ -25,6 +25,18 @@ SUBMIT_DIAGNOSTIC_DATA_TO_APP_DEVELOPERS=FALSE
 # To change this in your own script, comment out the FALSE
 # lines and uncomment the TRUE lines as appropriate.
 
+# Set the appropriate number value for AutoSubmitVersion
+# and ThirdPartyDataSubmitVersion by the OS version. For
+# 10.10.x, the value will be 4. For 10.11.x, the value will
+# be 5.
+
+if [[ ${osvers} -eq 10 ]]; then
+  VERSIONNUMBER=4
+elif [[ ${osvers} -ge 11 ]]; then
+  VERSIONNUMBER=5
+fi
+
+
 # Checks first to see if the Mac is running 10.10.0 or higher. 
 # If so, the desired diagnostic submission settings are applied.
 
@@ -39,9 +51,9 @@ if [[ ${osvers} -ge 10 ]]; then
   fi
 
  /usr/bin/defaults write "$CRASHREPORTER_SUPPORT"/DiagnosticMessagesHistory AutoSubmit -boolean ${SUBMIT_DIAGNOSTIC_DATA_TO_APPLE}
- /usr/bin/defaults write "$CRASHREPORTER_SUPPORT"/DiagnosticMessagesHistory AutoSubmitVersion -int 4
+ /usr/bin/defaults write "$CRASHREPORTER_SUPPORT"/DiagnosticMessagesHistory AutoSubmitVersion -int ${VERSIONNUMBER}
  /usr/bin/defaults write "$CRASHREPORTER_SUPPORT"/DiagnosticMessagesHistory ThirdPartyDataSubmit -boolean ${SUBMIT_DIAGNOSTIC_DATA_TO_APP_DEVELOPERS}
- /usr/bin/defaults write "$CRASHREPORTER_SUPPORT"/DiagnosticMessagesHistory ThirdPartyDataSubmitVersion -int 4
+ /usr/bin/defaults write "$CRASHREPORTER_SUPPORT"/DiagnosticMessagesHistory ThirdPartyDataSubmitVersion -int ${VERSIONNUMBER}
  /bin/chmod a+r "$CRASHREPORTER_SUPPORT"/DiagnosticMessagesHistory.plist
  /usr/sbin/chown root:admin "$CRASHREPORTER_SUPPORT"/DiagnosticMessagesHistory.plist
 
