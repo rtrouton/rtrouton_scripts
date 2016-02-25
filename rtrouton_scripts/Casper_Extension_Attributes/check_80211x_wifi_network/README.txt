b1583434fc6250584e5728a85e4ac1b3fe937b9b
Rich Trouton <rtrouton@yahoo.com>
2016-02-25 08:24:27 -0500

    Uploading Casper EA designed to check whether or not a specific 802.11x wireless network is available.
    Uploading Casper Extension Attribute designed to check whether or not a
    specific 802.11x wireless network is available on a Mac by checking the
    preferred wireless list and if the root CA for the 802.11x network is
    stored in /Library/Keychains/System.keychain

---
+++ rtrouton_scripts/Casper_Extension_Attributes/check_80211x_wifi_network/README.txt
@@ -0,0 +1,6 @@
+This Casper Extension Attribute is designed to identify whether or not a specific 802.11x wireless network is available on a Mac by checking the preferred wireless list and if the root CA for the 802.11x network is stored in /Library/Keychains/System.keychain
+
+If both the wireless network and root CA are available, the script returns "Yes"
+If both are not available, the script returns "No"
+
+See "Casper_Extension_Attribute_Setup.png" for a screenshot of how the Extension Attribute should be configured.
---
+++ rtrouton_scripts/Casper_Extension_Attributes/check_80211x_wifi_network/check_80211x_wifi_network.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+#
+# This extension attribute detects whether or not
+# a specific 802.11x wireless network is available
+# on a Mac by checking the preferred wireless list
+# and if the root CA for the 802.11x network is stored
+# in /Library/Keychains/System.keychain
+#
+
+# Enter the name of the wireless network
+# for the wifiNetwork variable
+
+wifiNetwork="Secure_WiFi_SSID_Here"
+
+# Enter the name of the root CA 
+# of the 802.11x network for the
+# rootCertificate variable
+
+rootCertificate="Certificate_Common_Name_Here"
+
+# Determines which OS the script is running on
+osvers=$(sw_vers -productVersion | awk -F. '{print $2}')
+
+# On 10.7 and higher, the Wi-Fi interface needs to be identified.
+# On 10.5 and 10.6, the Wi-Fi interface should be named as "AirPort"
+
+wifiDevice=`/usr/sbin/networksetup -listallhardwareports | awk '/^Hardware Port: Wi-Fi/,/^Ethernet Address/' | head -2 | tail -1 | cut -c 9-`
+
+if [[ ${osvers} -ge 7 ]]; then
+   wifiNetworkCheck=`networksetup -listpreferredwirelessnetworks $wifiDevice | grep "$wifiNetwork" | awk '{print $1}'`
+else
+   wifiNetworkCheck=`networksetup -listpreferredwirelessnetworks AirPort | grep "$wifiNetwork" | awk '{print $1}'`
+fi
+
+certificateCheck=`security find-certificate -c $rootCertificate /Library/Keychains/System.keychain 2>/dev/null | awk '/labl/' | cut -d'=' -f 2 | sed 's/"//g'` 
+  
+# Once the running OS is determined, the Mac is checked to see if the
+# preferred wireless network is listed and the root CA is installed in
+# /Library/Keychains/System.keychain
+
+if [[ "${wifiNetworkCheck}" == "" ]]; then
+    result="No"
+fi
+
+if [[ "${wifiNetworkCheck}" != "" ]] && [[ "${wifiNetworkCheck}" != "$wifiNetwork" ]]; then
+    result="No"
+fi
+
+if [[ "${wifiNetworkCheck}" == "$wifiNetwork" ]]; then
+   if [[ "${certificateCheck}" == "$rootCertificate" ]]; then
+        result="Yes"
+    else
+        result="No"
+   fi
+fi
+
+echo "<result>$result</result>"
