535ab93635eed4cd5dd0273628c4f35b6ccc2f35
Rich Trouton <rtrouton@yahoo.com>
2016-06-24 08:43:10 -0400

    Updating the report_IRK_public_key_hash script and Casper Extension attribute
    Updating the report_IRK_public_key_hash script and Casper Extension
    Attribute to handle reporting on the following OS conditions:
    
    10.12.x or later
    10.11.2 or later

--- rtrouton_scripts/report_IRK_public_key_hash/report_IRK_public_key_hash.sh
+++ rtrouton_scripts/report_IRK_public_key_hash/report_IRK_public_key_hash.sh
@@ -1,29 +1,22 @@
 #!/bin/bash
 
-IRKHashCheck(){
-
-# OS version check
-
 osvers_major=$(/usr/bin/sw_vers -productVersion | awk -F. '{print $1}')
 osvers_minor=$(/usr/bin/sw_vers -productVersion | awk -F. '{print $2}')
 osvers_dot_version=$(/usr/bin/sw_vers -productVersion | awk -F. '{print $3}')
 
-# Check to see if the OS version of the Mac includes a version of fdesetup which can output 
-# the SHA-1 hash of the institutional recovery key's public key in hexadecimal notation.
+IRKHashCheck(){
 
-if [[ ${osvers_major} -eq 10 ]] && [[ ${osvers_minor} -eq 11 ]] && [[ ${osvers_dot_version} -ge 2 ]]; then
-
-# If the OS check passes, the script continues on to see if the Mac is encrypted and if it is using
-# an institutional recovery key as a valid recovery key.
-#
+# This function checks for the SHA-1 hash of a FileVault 2 institutional recovery key's public 
+# key in hexadecimal notation.
+# 
 # If the Mac is not encrypted, the script sets the following string for the "result" value:
-#
-# "Not Available - Valid IRK Not Found" (no quotes)
+# 
+# "Not Available - Encryption Not Enabled" (no quotes)
 #
 # If the Mac is encrypted but is not using an institutional recovery key, the script sets the 
 # following string for the "result" value:
 #
-# "Not Available - Encryption Not Enabled" (no quotes)
+# "Not Available - Valid IRK Not Found" (no quotes)
 #
 # If the Mac is encrypted and an institutional recovery key is in use as a valid recovery key on 
 # the Mac's boot volume, the script sets the SHA-1 hash of the institutional recovery key's public 
@@ -40,7 +33,17 @@ if [[ ${osvers_major} -eq 10 ]] && [[ ${osvers_minor} -eq 11 ]] && [[ ${osvers_d
 	      fi
 	   elif [[ "$FDE" = "" ]]; then
            result="Not Available - Encryption Not Enabled"
-	   fi      
+	   fi
+}
+
+# Check to see if the OS version of the Mac includes a version of fdesetup which can output 
+# the SHA-1 hash of the institutional recovery key's public key in hexadecimal notation. If the 
+# OS check passes, run the IRKHashCheck function.
+
+if [[ ${osvers_major} -eq 10 ]] && [[ ${osvers_minor} -ge 12 ]]; then
+    IRKHashCheck
+elif [[ ${osvers_major} -eq 10 ]] && [[ ${osvers_minor} -eq 11 ]] && [[ ${osvers_dot_version} -ge 2 ]]; then 
+    IRKHashCheck
 else
 
 # If the OS check did not pass, the script sets the following string for the "result" value:
@@ -49,9 +52,5 @@ else
 
     result="Not Available - Unable To Export IRK Public Key Hash On `/usr/bin/sw_vers -productVersion`"
 fi
-
-}
-
-IRKHashCheck
 
 echo "$result"
