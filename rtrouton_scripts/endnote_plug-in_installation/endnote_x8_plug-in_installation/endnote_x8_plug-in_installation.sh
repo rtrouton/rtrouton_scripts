afa5b43c68101de059757a57266d818464be24f8
rtrouton <rtrouton@yahoo.com>
2018-01-26 11:06:53 -0500

    Updating endnote_x8_plug-in_installation.sh for Endnote X8.2
    Updating endnote_x8_plug-in_installation.sh for Endnote X8.2, to handle Word 15.x and 16.x properly.

--- rtrouton_scripts/endnote_plug-in_installation/endnote_x8_plug-in_installation/endnote_x8_plug-in_installation.sh
+++ rtrouton_scripts/endnote_plug-in_installation/endnote_x8_plug-in_installation/endnote_x8_plug-in_installation.sh
@@ -3,15 +3,17 @@
 
 #################################################################
 ##         Remove previous Endnote plug-ins from Office 2016   ##
-################################################################# 
+#################################################################
 
 if [[ -e "/Library/Application Support/Microsoft/Office365/User Content.localized/Startup.localized/Word/EndNote CWYW Word 2016.bundle" ]]; then
    /bin/rm -rf "/Library/Application Support/Microsoft/Office365/User Content.localized/Startup.localized/Word/EndNote CWYW Word 2016.bundle"
+elif [[ -e "/Library/Application Support/Microsoft/Office365/User Content.localized/Startup.localized/Word/EndNote CWYW Word 16.bundle" ]]; then
+   /bin/rm -rf "/Library/Application Support/Microsoft/Office365/User Content.localized/Startup.localized/Word/EndNote CWYW Word 16.bundle"
 fi
 
 #################################################################
 ##         Remove previous Endnote plug-ins from Office 2011   ##
-################################################################# 
+#################################################################
 
 if [[ -e "/Applications/Microsoft Office 2011/Office/Startup/Word/EndNote CWYW Word 2011.bundle" ]]; then
    /bin/rm -rf "/Applications/Microsoft Office 2011/Office/Startup/Word/EndNote CWYW Word 2011.bundle"
@@ -19,7 +21,7 @@ fi
 
 #################################################################
 ##         Remove previous Endnote plug-ins from Office 2008   ##
-################################################################# 
+#################################################################
 
 if [[ -e "/Applications/Microsoft Office 2008/Office/Startup/Word/EndNote CWYW Word 2008.bundle" ]]; then
    /bin/rm -rf "/Applications/Microsoft Office 2008/Office/Startup/Word/EndNote CWYW Word 2008.bundle"
@@ -40,13 +42,25 @@ if [[ -e "/Applications/Microsoft Word.app" ]] && [[ ! -e "/Library/Application 
    /bin/echo "Creating necessary support directories for Endnote X8 Cite While You Write plug-ins for Word 2016."
 fi
 
+###########################################################################################################################
+##    This checks for if the version of Office 2016 is 15.x or 16.x, as Endnote has different Word Plug-ins for each     ##
+###########################################################################################################################
+
+if [[ -e "/Applications/Microsoft Word.app" ]]; then
+   WordVersion=$(/usr/bin/defaults read "/Applications/Microsoft Word.app/Contents/Info" CFBundleShortVersionString | awk -F. '{print $1}')
+fi
+
 ########################################################################
 ##    This re-copies the Endnote X8 Plug-in for Office 2016          ##
 ########################################################################
 
 if [[ -e "/Library/Application Support/Microsoft/Office365/User Content.localized/Startup.localized/Word/" ]]; then
-   /usr/bin/ditto "/Applications/EndNote X8/Cite While You Write/EndNote CWYW Word 2016.bundle"  "/Library/Application Support/Microsoft/Office365/User Content.localized/Startup.localized/Word/EndNote CWYW Word 2016.bundle"
-   /usr/sbin/chown -R root:admin "/Library/Application Support/Microsoft/Office365/User Content.localized/Startup.localized/Word" 
+   if [[ "$WordVersion" = 15 ]] && [[ -e "/Applications/EndNote X8/Cite While You Write/EndNote CWYW Word 2016.bundle" ]]; then
+       /usr/bin/ditto "/Applications/EndNote X8/Cite While You Write/EndNote CWYW Word 2016.bundle"  "/Library/Application Support/Microsoft/Office365/User Content.localized/Startup.localized/Word/EndNote CWYW Word 2016.bundle"
+   elif [[ "$WordVersion" = 16 ]] && [[ -e "/Applications/EndNote X8/Cite While You Write/EndNote CWYW Word 16.bundle" ]]; then
+       /usr/bin/ditto "/Applications/EndNote X8/Cite While You Write/EndNote CWYW Word 16.bundle"  "/Library/Application Support/Microsoft/Office365/User Content.localized/Startup.localized/Word/EndNote CWYW Word 16.bundle"
+   fi
+   /usr/sbin/chown -R root:admin "/Library/Application Support/Microsoft/Office365/User Content.localized/Startup.localized/Word"
    /bin/echo "Copying Endnote X8 Cite While You Write plug-ins for Word 2016."
 fi
 
@@ -56,7 +70,7 @@ fi
 
 if [[ -e "/Applications/Microsoft Office 2011/Office/Startup/Word" ]]; then
    /usr/bin/ditto "/Applications/EndNote X8/Cite While You Write/EndNote CWYW Word 2011.bundle"  "/Applications/Microsoft Office 2011/Office/Startup/Word/EndNote CWYW Word 2011.bundle"
-   /usr/sbin/chown -R root:admin "/Applications/Microsoft Office 2011/Office/Startup/Word" 
+   /usr/sbin/chown -R root:admin "/Applications/Microsoft Office 2011/Office/Startup/Word"
    /bin/echo "Copying Endnote X8 Cite While You Write plug-ins for Word 2011."
 fi
 
@@ -66,7 +80,7 @@ fi
 ########################################################################
 
 if [[ -e "/Applications/Microsoft Office 2008/Office/Startup/Word" ]]; then
-   /usr/bin/ditto "/Applications/EndNote X8/Cite While You Write/EndNote CWYW Word 2008.bundle"  "/Applications/Microsoft Office 2008/Office/Startup/Word/EndNote CWYW Word 2008.bundle" 
-   /usr/sbin/chown -R root:admin "/Applications/Microsoft Office 2008/Office/Startup/Word" 
+   /usr/bin/ditto "/Applications/EndNote X8/Cite While You Write/EndNote CWYW Word 2008.bundle"  "/Applications/Microsoft Office 2008/Office/Startup/Word/EndNote CWYW Word 2008.bundle"
+   /usr/sbin/chown -R root:admin "/Applications/Microsoft Office 2008/Office/Startup/Word"
    /bin/echo "Copying Endnote X8 Cite While You Write plug-ins for Word 2008."
 fi
