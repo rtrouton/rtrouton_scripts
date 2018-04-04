00155a5f100d1d7ac3586ede0d8bed23496196cb
rtrouton <rtrouton@yahoo.com>
2018-04-04 11:12:56 -0400

    Uploading disable_apple_icloud_diagnostic_and_siri_pop_ups/disable_apple_icloud_data_privacy_diagnostic_and_siri_pop_ups script to GitHub.
    Uploading disable_apple_icloud_diagnostic_and_siri_pop_ups/disable_apple_icloud_data_privacy_diagnostic_and_siri_pop_ups script to GitHub. Also uploading a payload-free package which runs the script.

--- rtrouton_scripts/disable_apple_icloud_diagnostic_and_siri_pop_ups/README.md
+++ rtrouton_scripts/disable_apple_icloud_diagnostic_and_siri_pop_ups/README.md
@@ -1,7 +1,12 @@
-Starting in 10.7.2, Apple set the iCloud sign-in to pop up on the first login. In 10.10, Apple added a new Diagnostic window that pops up at first login after the iCloud sign-in. In 10.12, Apple added another new pop-up window for Siri. 
+Starting in 10.7.2, Apple has added several pop-up windows which appear at the first login of a new account:
 
-Since having this appear may not be desirable in all Mac environments, it makes sense to be able to turn the the iCloud, Diagnostic and Siri pop-up windows off for new user accounts. 
+* Mac OS X 10.7.2: iCloud sign-in window
+* OS X 10.10: Diagnostic agreement window
+* macOS 10.12: Siri setup window
+* macOS 10.13.4: Data & Privacy information window 
 
-Apple is using `~/Library/Preferences/com.apple.SetupAssistant.plist` to store the settings that indicate whether or not the iCloud, Diagnostic and Siri agreement processes have run. Building on work done by the folks behind DeployStudio, I've built a script that pre-sets those values for new and existing accounts on a particular Mac. In turn, that should stop the iCloud, Diagnostic and Siri pop-up messages from appearing on that Mac.
+Since having this appear may not be desirable in all Mac environments, it makes sense to be able to turn the the iCloud, Data & Privacy, Diagnostic and Siri pop-up windows off for new user accounts. 
 
-This script is also available as a payload-free package, available for download from the **payload_free_package** directory.
+Apple is using `~/Library/Preferences/com.apple.SetupAssistant.plist` to store the settings that indicate whether or not the iCloud, Data & Privacy, Diagnostic and Siri agreement processes have run. Building on work done by the folks behind DeployStudio, I've built a script that pre-sets those values for new and existing accounts on a particular Mac. In turn, that should stop the iCloud, Data & Privacy Diagnostic and Siri pop-up messages from appearing on that Mac.
+
+This script is also available as a payload-free package, available for download from the `payload_free_package` directory.
--- rtrouton_scripts/disable_apple_icloud_diagnostic_and_siri_pop_ups/payload-free_package_script/disable_apple_icloud_data_privacy_diagnostic_and_siri_pop_ups.sh
+++ rtrouton_scripts/disable_apple_icloud_diagnostic_and_siri_pop_ups/payload-free_package_script/disable_apple_icloud_data_privacy_diagnostic_and_siri_pop_ups.sh
@@ -8,11 +8,11 @@ sw_vers=$(sw_vers -productVersion)
 
 sw_build=$(sw_vers -buildVersion)
 
-# Checks first to see if the Mac is running 10.7.0 or higher. 
+# Checks first to see if the Mac is running 10.7.0 or higher.
 # If so, the script checks the system default user template
 # for the presence of the Library/Preferences directory. Once
-# found, the iCloud, Diagnostic and Siri pop-up settings are set 
-# to be disabled.
+# found, the iCloud, Data & Privacy, Diagnostic and Siri pop-up
+# settings are set to be disabled.
 
 if [[ ${osvers} -ge 7 ]]; then
 
@@ -22,15 +22,17 @@ if [[ ${osvers} -ge 7 ]]; then
     /usr/bin/defaults write "${USER_TEMPLATE}"/Library/Preferences/com.apple.SetupAssistant GestureMovieSeen none
     /usr/bin/defaults write "${USER_TEMPLATE}"/Library/Preferences/com.apple.SetupAssistant LastSeenCloudProductVersion "${sw_vers}"
     /usr/bin/defaults write "${USER_TEMPLATE}"/Library/Preferences/com.apple.SetupAssistant LastSeenBuddyBuildVersion "${sw_build}"
-    /usr/bin/defaults write "${USER_TEMPLATE}"/Library/Preferences/com.apple.SetupAssistant DidSeeSiriSetup -bool TRUE      
+    /usr/bin/defaults write "${USER_TEMPLATE}"/Library/Preferences/com.apple.SetupAssistant DidSeePrivacy -bool TRUE
+    /usr/bin/defaults write "${USER_TEMPLATE}"/Library/Preferences/com.apple.SetupAssistant DidSeeSiriSetup -bool TRUE
   done
-  
+
  # Checks first to see if the Mac is running 10.7.0 or higher.
  # If so, the script checks the existing user folders in /Users
  # for the presence of the Library/Preferences directory.
  #
  # If the directory is not found, it is created and then the
- # iCloud, Diagnostic and Siri pop-up settings are set to be disabled.
+ # iCloud, Data & Privacy, Diagnostic and Siri pop-up settings
+ # are set to be disabled.
 
  for USER_HOME in "$3/Users"/*
   do
@@ -46,6 +48,7 @@ if [[ ${osvers} -ge 7 ]]; then
         /usr/bin/defaults write "${USER_HOME}"/Library/Preferences/com.apple.SetupAssistant GestureMovieSeen none
         /usr/bin/defaults write "${USER_HOME}"/Library/Preferences/com.apple.SetupAssistant LastSeenCloudProductVersion "${sw_vers}"
         /usr/bin/defaults write "${USER_HOME}"/Library/Preferences/com.apple.SetupAssistant LastSeenBuddyBuildVersion "${sw_build}"
+        /usr/bin/defaults write "${USER_HOME}"/Library/Preferences/com.apple.SetupAssistant DidSeePrivacy -bool TRUE
         /usr/bin/defaults write "${USER_HOME}"/Library/Preferences/com.apple.SetupAssistant DidSeeSiriSetup -bool TRUE
         /usr/sbin/chown "${USER_UID}" "${USER_HOME}"/Library/Preferences/com.apple.SetupAssistant.plist
       fi
