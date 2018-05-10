03ba216f9d920df8aafcd7bd31beca8cb95f436e
rtrouton <rtrouton@yahoo.com>
2018-05-10 15:21:16 -0400

    Uploading detect_missing_secure_token.sh and complementary extension attribute
    Uploading detect_missing_secure_token.sh, complementary extension attribute and support files to GitHub.

---
+++ rtrouton_scripts/Casper_Extension_Attributes/detect_missing_secure_token/README.md
@@ -0,0 +1,28 @@
+This Jamf Pro Extension Attribute is designed to report if the current logged-in user has a Secure Token attribute associated with their account. See `Jamf_Pro_Extension_Attribute_Setup.png` for a screenshot of how the Extension Attribute should be configured.
+
+![Jamf_Pro_Extension_Attribute_Setup.png](Jamf_Pro_Extension_Attribute_Setup.png)
+
+The Extension Attribute checks for the following:
+
+1. If the Mac is running 10.13.x or later.
+2. If the boot drive is using Apple File System (APFS) for its filesystem.
+3. If FileVault is enabled or not.
+
+If the Mac passes the following checks:
+
+* Running 10.13.0 or later
+* The boot drive is using APFS
+* FileVault is enabled
+
+Then the following actions take place:
+
+1. The logged-in user is checked to see if it can be determined.
+2. If the logged-in user can be determined and it is not the `root` user, the `sysadminctl` tool is used to check to see if the account has the Secure Token attribute associated with it.
+
+If the logged-in user account should have a Secure Token attribute associated with it and does not, the Extension Attribute will report the following:
+
+`1` 
+
+Any other outcome, the Extension Attribute will report the following:
+
+`0`
---
+++ rtrouton_scripts/Casper_Extension_Attributes/detect_missing_secure_token/detect_missing_secure_token.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# Jamf Pro Extension Attribute which reports if the
+# current logged-in user has a Secure Token attribute
+# associated with their account.
+
+osvers_minor=$(/usr/bin/sw_vers -productVersion | /usr/bin/awk -F. '{print $2}')
+result=0
+
+MissingSecureTokenCheck() {
+
+	# Get the currently logged-in user and go ahead if not root.
+
+	current_user=$(/bin/ls -l /dev/console | /usr/bin/awk '{ print $3 }')
+
+	# This function checks if the logged-in user has Secure Token attribute associated
+	# with their account. If the token_status variable returns "0", then the following
+	# status is returned from the Extension Attribute:
+	#
+	# 1
+	#
+	# If anything else is returned, the following status is
+	# returned from the Extension Attribute:
+	#
+	# 0
+
+	if [[ -n "$current_user" && "$current_user" != "root" ]]; then
+
+	  # Get the Secure Token status.
+
+		token_status=$(/usr/sbin/sysadminctl -adminUser "" -adminPassword "" -secureTokenStatus "$current_user" 2>&1 | /usr/bin/grep -ic enabled)
+
+		# If there is no secure token associated with the logged-in account,
+		# the token_status variable should return "0".
+
+		if [[ "$token_status" -eq 0 ]]; then
+			result=1
+		fi
+	fi
+
+	# If unable to determine the logged-in user
+	# or if the logged-in user is root, then the following
+	# status is returned from the Extension Attribute:
+	#
+	# 0
+}
+
+# Check to see if the OS version of the Mac supports running APFS boot volumes.
+
+if [[ ${osvers_minor} -ge 13 ]]; then
+
+	# If the OS check passes, check to see if the boot volume has an APFS filesystem
+	# with FileVault turned on.
+
+	if [[ $(/usr/sbin/diskutil info / | /usr/bin/awk '/Type \(Bundle\)/ {print $3}') = "apfs" && $(/usr/bin/fdesetup status | /usr/bin/grep -io "is on") ]]; then
+
+		# If the boot volume is using APFS for its filesystem and FileVault is on,
+		# run the MissingSecureTokenCheck function.
+		MissingSecureTokenCheck
+	fi
+
+	# If the OS, filesystem or encryption check did not pass, the script sets the following string for the "result" value:
+	#
+	# 0
+fi
+
+echo "<result>$result</result>"
+
+exit 0
