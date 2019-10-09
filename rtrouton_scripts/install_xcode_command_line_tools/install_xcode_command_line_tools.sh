6927f7d8940ff8ecd94702cfb9bd833edaf1c53b
Rich Trouton <rtrouton@yahoo.com>
2019-10-08 22:04:36 -0400

    Update install_xcode_command_line_tools.sh

--- rtrouton_scripts/install_xcode_command_line_tools/install_xcode_command_line_tools.sh
+++ rtrouton_scripts/install_xcode_command_line_tools/install_xcode_command_line_tools.sh
@@ -2,12 +2,12 @@
 
 # Installing the Xcode command line tools on 10.7.x or higher
 
-osx_vers=$(sw_vers -productVersion | awk -F "." '{print $2}')
+macos_vers=$(sw_vers -productVersion | awk -F "." '{print $2}')
 cmd_line_tools_temp_file="/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress"
 
 # Installing the latest Xcode command line tools on 10.9.x or higher
 
-if [[ "$osx_vers" -ge 9 ]]; then
+if [[ "$macos_vers" -ge 9 ]]; then
 
 	# Create the placeholder file which is checked by the softwareupdate tool 
 	# before allowing the installation of the Xcode command line tools.
@@ -15,10 +15,12 @@ if [[ "$osx_vers" -ge 9 ]]; then
 	touch "$cmd_line_tools_temp_file"
 	
 	# Identify the correct update in the Software Update feed with "Command Line Tools" in the name for the OS version in question.
-	
-	if [[ "$osx_vers" -gt 9 ]]; then
-	   cmd_line_tools=$(softwareupdate -l | awk '/\*\ Command Line Tools/ { $1=$1;print }' | grep "$osx_vers" | sed 's/^[[ \t]]*//;s/[[ \t]]*$//;s/*//' | cut -c 2-)
-	elif [[ "$osx_vers" -eq 9 ]]; then
+
+	if [[ "$macos_vers" -ge 15 ]]; then
+	   cmd_line_tools=$(softwareupdate -l | awk '/\*\ Label: Command Line Tools/ { $1=$1;print }' | sed 's/^[[ \t]]*//;s/[[ \t]]*$//;s/*//' | cut -c 9-)	
+	elif [[ "$macos_vers" -gt 9 ]] && [[ "$macos_vers" -lt 14 ]]; then
+	   cmd_line_tools=$(softwareupdate -l | awk '/\*\ Command Line Tools/ { $1=$1;print }' | grep "$macos_vers" | sed 's/^[[ \t]]*//;s/[[ \t]]*$//;s/*//' | cut -c 2-)
+	elif [[ "$macos_vers" -eq 9 ]]; then
 	   cmd_line_tools=$(softwareupdate -l | awk '/\*\ Command Line Tools/ { $1=$1;print }' | grep "Mavericks" | sed 's/^[[ \t]]*//;s/[[ \t]]*$//;s/*//' | cut -c 2-)
 	fi
 	
@@ -48,13 +50,13 @@ fi
 # instead from public download URLs, which can be found in the dvtdownloadableindex:
 # https://devimages.apple.com.edgekey.net/downloads/xcode/simulators/index-3905972D-B609-49CE-8D06-51ADC78E07BC.dvtdownloadableindex
 
-if [[ "$osx_vers" -eq 7 ]] || [[ "$osx_vers" -eq 8 ]]; then
+if [[ "$macos_vers" -eq 7 ]] || [[ "$macos_vers" -eq 8 ]]; then
 
-	if [[ "$osx_vers" -eq 7 ]]; then
+	if [[ "$macos_vers" -eq 7 ]]; then
 	    DMGURL=http://devimages.apple.com/downloads/xcode/command_line_tools_for_xcode_os_x_lion_april_2013.dmg
 	fi
 	
-	if [[ "$osx_vers" -eq 8 ]]; then
+	if [[ "$macos_vers" -eq 8 ]]; then
 	     DMGURL=http://devimages.apple.com/downloads/xcode/command_line_tools_for_osx_mountain_lion_april_2014.dmg
 	fi
 
