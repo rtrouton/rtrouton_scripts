1a5021c7be2c5c1be6c33aa2dcdd591c401362a4
Rich Trouton <rtrouton@yahoo.com>
2020-03-20 13:42:08 -0400

    Update disable_visual_studio_code_telemetry.sh

--- rtrouton_scripts/disable_visual_studio_code_telemetry/disable_visual_studio_code_telemetry.sh
+++ rtrouton_scripts/disable_visual_studio_code_telemetry/disable_visual_studio_code_telemetry.sh
@@ -4,7 +4,7 @@
 
 exitCode=0
 
-# This script uses jq, which is a tool for working with JSON files. 
+# This script uses jq, which is a tool for working with JSON files.
 # It is not natively available as part of macOS, so it needs to be
 # installed separately.
 #
@@ -19,53 +19,51 @@ CURRENT_USER=$(/bin/ls -l /dev/console | /usr/bin/awk '{ print $3 }')
 
 if [[ -n "$CURRENT_USER" && "$CURRENT_USER" != "root" ]]; then
 
-    USER_HOME=$(/usr/bin/dscl . -read "/Users/$CURRENT_USER" NFSHomeDirectory | /usr/bin/sed 's/^[^\/]*//g')
+  USER_HOME=$(/usr/bin/dscl . -read "/Users/$CURRENT_USER" NFSHomeDirectory | /usr/bin/sed 's/^[^\/]*//g')
 
-			# If jq is executable, proceed with script.
-			# Otherwise halt and log an error.
-			
-			if [[ -x "$jq" ]]; then
-			
-			# If an existing settings.json file exists for Visual Studio Code,
-			# update it with the desired telemetry setting.
-			
-			  if [[ -f "$USER_HOME/Library/Application Support/Code/User/settings.json" ]]; then
-			     vscode_settings="$USER_HOME/Library/Application Support/Code/User/settings.json"
-			     updated_vscode_settings=$("$jq" '."telemetry.enableTelemetry" = false' < "$vscode_settings") 
-			     echo "${updated_vscode_settings}" > "$vscode_settings"
-			     /usr/sbin/chown -R "$CURRENT_USER" "$vscode_settings"
-			  else
-			
-			# If an existing settings.json file does not yet exist for Visual Studio Code,
-			# create the settings file with the desired telemetry setting.
+  # If jq is executable, proceed with script.
+  # Otherwise halt and log an error.
 
-			  if [[ ! -d "$USER_HOME/Library/Application Support/Code/User" ]]; then
-			     sudo -u "$CURRENT_USER" /bin/mkdir -p "$USER_HOME/Library/Application Support/Code/User"
-			  fi
-			     vscode_settings="$USER_HOME/Library/Application Support/Code/User/settings.json"
-			     /usr/bin/touch "$vscode_settings"
-			     echo "{}" >> "$vscode_settings"
-			     vscode_tmp=$(mktemp)
-			     "$jq" -M '. +{ "telemetry.enableTelemetry": false }' "$vscode_settings" > "vscode_tmp"
-			     /bin/mv "vscode_tmp" "$vscode_settings"
-			     /usr/sbin/chown -R "$CURRENT_USER" "$vscode_settings"
-			  fi
-			else
-              echo "jq not executable!"
-              exitCode=1
-            fi
-            		     
-            # Verify that the desired setting is in place
-            
-            vscode_settings="$USER_HOME/Library/Application Support/Code/User/settings.json"
-            if [[ $("$jq" '."telemetry.enableTelemetry"' "$vscode_settings") = "false" ]]; then
-			     echo "Telemetry disabled."
-		    else
-			     echo "Unable to verify that telemetry is disabled!"
-			     exitCode=1
-			fi
-		fi
-	fi
+  if [[ -x "$jq" ]]; then
+
+    # If an existing settings.json file exists for Visual Studio Code,
+    # update it with the desired telemetry setting.
+
+    if [[ -f "$USER_HOME/Library/Application Support/Code/User/settings.json" ]]; then
+      vscode_settings="$USER_HOME/Library/Application Support/Code/User/settings.json"
+      updated_vscode_settings=$("$jq" '."telemetry.enableTelemetry" = false' <"$vscode_settings")
+      echo "${updated_vscode_settings}" >"$vscode_settings"
+      /usr/sbin/chown -R "$CURRENT_USER" "$vscode_settings"
+    else
+
+      # If an existing settings.json file does not yet exist for Visual Studio Code,
+      # create the settings file with the desired telemetry setting.
+
+      if [[ ! -d "$USER_HOME/Library/Application Support/Code/User" ]]; then
+        sudo -u "$CURRENT_USER" /bin/mkdir -p "$USER_HOME/Library/Application Support/Code/User"
+      fi
+      vscode_settings="$USER_HOME/Library/Application Support/Code/User/settings.json"
+      /usr/bin/touch "$vscode_settings"
+      echo "{}" >>"$vscode_settings"
+      vscode_tmp=$(mktemp)
+      "$jq" -M '. +{ "telemetry.enableTelemetry": false }' "$vscode_settings" >"vscode_tmp"
+      /bin/mv "vscode_tmp" "$vscode_settings"
+      /usr/sbin/chown -R "$CURRENT_USER" "$vscode_settings"
+    fi
+  else
+    echo "jq not executable!"
+    exitCode=1
+  fi
+
+  # Verify that the desired setting is in place
+
+  vscode_settings="$USER_HOME/Library/Application Support/Code/User/settings.json"
+  if [[ $("$jq" '."telemetry.enableTelemetry"' "$vscode_settings") = "false" ]]; then
+    echo "Telemetry disabled."
+  else
+    echo "Unable to verify that telemetry is disabled!"
+    exitCode=1
+  fi
 fi
 
 exit "$exitCode"
