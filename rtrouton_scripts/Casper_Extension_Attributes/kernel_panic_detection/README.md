45c68e5507d20ffc85ee55e8a1863617c03f645c
Rich Trouton <rtrouton@yahoo.com>
2020-11-10 11:00:38 -0500

    Uploading kernel panic detection Extension Attribute
    Uploading kernel panic detection Extension Attribute and associated README file.

---
+++ rtrouton_scripts/Casper_Extension_Attributes/kernel_panic_detection/README.md
@@ -0,0 +1,5 @@
+This Jamf Pro Extension Attribute is designed to check the contents of `/Library/Logs/DiagnosticReports` and report on how many log files with the file suffix of `.panic` were created in the previous seven days.
+
+See below for a screenshot of how the Extension Attribute should be configured.
+
+![](Jamf_Pro_Extension_Attribute.png)
---
+++ rtrouton_scripts/Casper_Extension_Attributes/kernel_panic_detection/kernel_panic_detection.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Detects kernel panics which occurred in the last seven days.
+#
+# Original idea and script from here:
+# https://www.jamf.com/jamf-nation/discussions/23976/kernal-panic-reporting#responseChild145035
+#
+# This Jamf Pro Extension Attribute is designed to 
+# check the contents of /Library/Logs/DiagnosticReports
+# and report on how many log files with the file suffix
+# of ".panic" were created in the previous seven days.
+
+PanicLogCount=$(/usr/bin/find /Library/Logs/DiagnosticReports -Btime -7 -name *.panic | grep . -c)
+
+echo "<result>$PanicLogCount</result>"
+
+exit 0
