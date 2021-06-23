23c5942995b58e1cc27834646ba8732de6f4333c
Rich Trouton <rich.trouton@sap.com>
2021-06-23 15:25:57 -0400

    Update Jamf_Pro_Self_Service_Bookmark_Backup.sh
    Script now parses ~/Library/Application Support/com.jamfsoftware.selfservice.mac/CocoaAppCD.storedata for Self Service bookmark information.

--- rtrouton_scripts/Casper_Scripts/Jamf_Pro_Self_Service_Bookmark_Backup/Jamf_Pro_Self_Service_Bookmark_Backup.sh
+++ rtrouton_scripts/Casper_Scripts/Jamf_Pro_Self_Service_Bookmark_Backup/Jamf_Pro_Self_Service_Bookmark_Backup.sh
@@ -21,23 +21,53 @@ error=0
 
 if [[ -z "$SelfServiceBookmarkBackupDirectory" ]]; then
    SelfServiceBookmarkBackupDirectory=$(mktemp -d)
-   echo "A location to store copied bookmarks has not been specified."
-   echo "Copied bookmarks will be stored in $SelfServiceBookmarkBackupDirectory."
+   echo "A location to store downloaded groups has not been specified."
+   echo "Downloaded groups will be stored in $SelfServiceBookmarkBackupDirectory."
 fi
 
-self_service_bookmarks="/Library/Application Support/JAMF/Self Service/Managed Plug-ins"
+self_service_bookmark_file="$HOME/Library/Application Support/com.jamfsoftware.selfservice.mac/CocoaAppCD.storedata"
 
-for bookmark in "$self_service_bookmarks"/*.plist
-do
-  echo "Processing "$bookmark" file..."
-  bookmark_name=$(/usr/bin/defaults read "$bookmark" title)
-  cat "$bookmark" > "$SelfServiceBookmarkBackupDirectory/${bookmark_name}.plist"
-  if [[ $? -eq 0 ]]; then
-     echo "$bookmark_name.plist processed and stored in $SelfServiceBookmarkBackupDirectory."
-  else
-     echo "ERROR! Problem occurred when processing $self_service_bookmarks/$bookmark file!"
-     error=1
-  fi
-done
+if [[ -f "$self_service_bookmark_file" ]]; then
+    tmp_dir="/private/tmp/bookmark-workdir-$(date +%y%m%d%H%M%S)"
+    mkdir -p "$tmp_dir"
+    
+    # For the next command, add a trailing slash for
+    # the the tmp_dir variable if it's not there.
+    
+    length=${#tmp_dir}
+    last_char=${tmp_dir:length-1:1}
+    [[ $last_char != "/" ]] && tmp_dir="$tmp_dir/";
+    
+    sed -n '/SSBOOKMARK/,/object/p' "$self_service_bookmark_file" | awk -v a=$tmp_dir '/SSBOOKMARK/{filename=a""++i".xml"}; {print >filename}' -
+    
+    #remove trailing slash if needed from the bookmark and tmp directories
+    
+    SelfServiceBookmarkBackupDirectory=${SelfServiceBookmarkBackupDirectory%%/}
+    tmp_dir=${tmp_dir%%/}
+    
+    for file in "$tmp_dir"/*
+    do
+    
+      # Add XML declaration to first line if not already present in the file.
+      # This will allow xmllint to format the XML in human-readable format.
+    
+      if [[ -z $(cat $file | grep "<?xml version="1.0" encoding="UTF-8"?>") ]]; then
+         echo -e "<?xml version="\""1.0"\"" encoding="\""UTF-8"\""?>\n$(cat $file)" > $file
+      fi
+    
+      bookmark_name=$(cat $file | awk -F '[<>]' '/"name"/{print $3}')
+      xmllint --format "$file" > "$file"_formatted.xml
+      mv "$file"_formatted.xml "$SelfServiceBookmarkBackupDirectory/$bookmark_name".xml
+      if [[ $? -eq 0 ]]; then
+         echo "$bookmark_name.xml processed and stored in $SelfServiceBookmarkBackupDirectory."
+      else
+         echo "ERROR! Problem occurred when processing $self_service_bookmark_file file!"
+         error=1
+      fi
+      
+    done
+    
+    rm -rf "$tmp_dir"
+fi
 
 exit $error
