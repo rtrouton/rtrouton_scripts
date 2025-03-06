22e34ebb93e0e4e347e8aaeec9431925be0cf51c
rtrouton <rtrouton@gmail.com>
2025-03-06 14:42:46 -0500

    Uploading Extension Attributes which determine where a specified Microsoft app should be receiving app updates from
    Uploading Extension Attributes, along with associated README files, which determine where a specified Microsoft app should be receiving app updates from, with one of the following sources:
    
    Mac App Store (MAS)
    Volume Purchase Program (VPP)
    Microsoft AutoUpdate (MAU)

---
+++ rtrouton_scripts/Casper_Extension_Attributes/Microsoft_Excel_Update_Source/Microsoft_Excel_Update_Source.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Jamf Pro Extension Attribute to determine which tool should be updating an installed
+# copy of Microsoft Excel.
+# 
+# If the app was installed using the Mac App Store with a person's Apple Account, the EA 
+# will return the following result:
+#
+# MAS
+#
+# If the app was installed using the Volume Purchase Program and installed by an MDM, 
+# the EA will return the following result:
+#
+# VPP
+#
+# If the app was not installed via either the Mac App Store or VPP, the EA 
+# will return the following result:
+#
+# MAU
+#
+# Note: "MAU" stands for Microsoft AutoUpdate, Microsoft's tool for updating
+# Microsoft apps on macOS.
+#
+# In all other cases, the EA will return the following result:
+#
+# NA
+
+result="NA"
+app_name="Microsoft Excel.app"
+app_install_path="/Applications/$app_name"
+
+
+if [[ -d "${app_install_path}" ]]; then
+    app_install_source=$(/usr/bin/mdls -name kMDItemAppStoreReceiptType "${app_install_path}" | awk '{print $3}' | tr -d '"')
+    if [[ "$app_install_source" = "ProductionVPP" ]]; then
+         result="VPP"
+    elif [[ "$app_install_source" = "Production" ]]; then
+            result="MAS"
+    else
+        result="MAU"
+    fi
+fi
+
+echo "<result>$result</result>"
+exit 0
---
+++ rtrouton_scripts/Casper_Extension_Attributes/Microsoft_Excel_Update_Source/README.md
@@ -0,0 +1,24 @@
+Jamf Pro Extension Attribute used to determine which tool should be updating an installed
+copy of Microsoft Excel.
+
+If the app was installed using the Mac App Store with a person's Apple Account, the EA will return the following result:
+
+`MAS`
+
+If the app was installed using the Volume Purchase Program and installed by an MDM, the EA will return the following result:
+
+`VPP`
+
+If the app was not installed via either the Mac App Store or VPP, the EA will return the following result:
+
+`MAU`
+
+**Note:** `MAU` stands for Microsoft AutoUpdate, Microsoft's tool for updating Microsoft apps on macOS.
+
+In all other cases, the EA will return the following result:
+
+`NA`
+
+See `Jamf_Pro_Extension_Attribute_Setup.png` for a screenshot of how the Extension Attribute should be configured.
+
+![Jamf_Pro_Extension_Attribute_Setup.png](Jamf_Pro_Extension_Attribute_Setup.png)
