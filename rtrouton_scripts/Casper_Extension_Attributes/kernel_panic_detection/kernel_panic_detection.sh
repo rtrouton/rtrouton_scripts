d5920944ad39f98e8b5a34318d026c380ff8caad
rtrouton <rtrouton@gmail.com>
2025-03-10 10:32:09 -0400

    Update kernel_panic_detection.sh
    Updated kernel_panic_detection.sh to exclude hidden files which include ".panic" in their filename.

--- rtrouton_scripts/Casper_Extension_Attributes/kernel_panic_detection/kernel_panic_detection.sh
+++ rtrouton_scripts/Casper_Extension_Attributes/kernel_panic_detection/kernel_panic_detection.sh
@@ -10,7 +10,7 @@
 # and report on how many log files with the file suffix
 # of ".panic" were created in the previous seven days.
 
-PanicLogCount=$(/usr/bin/find /Library/Logs/DiagnosticReports -Btime -7 -name *.panic | grep . -c)
+PanicLogCount=$(/usr/bin/find /Library/Logs/DiagnosticReports -not -path '*/\.*' -Btime -7 -name *.panic | grep . -c)
 
 echo "<result>$PanicLogCount</result>"
 
